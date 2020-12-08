363a970ef8bc847cad6f935351a930bf8e3833fb
AdoNunes <adonay.s.nunes@gmail.com>
2020-12-08 08:19:04 -0500

    iphone and camera streams

---
+++ io/iphone.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+import time
+
+position = pyautogui.position()
+
+def click_iphone():
+    pyautogui.click(x=3403, y=1204, button='left', clicks=3, interval=0.1)
+    time.sleep(5)
+
+click_iphone()
+
+pyautogui.moveTo(position[0], position[1])
