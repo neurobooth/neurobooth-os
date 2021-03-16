e88b456f145a559cb606e7f55265fdb9649174d7
adonunes <adonay.s.nunes@gmail.com>
2021-03-16 16:09:58 -0400

    LSL mic capture class

--- iout/microphone.py
+++ iout/microphone.py
@@ -1,35 +1,49 @@
 from pylsl import StreamInfo, StreamOutlet
 import pyaudio
 import numpy as np
+import threading
 
 
 
-def audio_stream(FORMAT=pyaudio.paFloat32,
-                 CHANNELS=1,
-                 RATE=44100,
-                 CHUNK=1024,
-                 SAMPLE_RATE=200):
+class MicStream():
+    def __init__(self, CHANNELS=1, RATE=44100,  CHUNK=1024, SAMPLE_RATE=200,
+                 FORMAT=pyaudio.paFloat32):
+       
+        self.CHUNK = CHUNK
+        
+        # Create audio object
+        audio = pyaudio.PyAudio()
     
-    # Create audio object
-    audio = pyaudio.PyAudio()
+        # Create stream
+        self.stream = audio.open(format=FORMAT,
+                                 channels=CHANNELS,
+                                 rate=RATE,
+                                 input=True,
+                                 output=True,
+                                 frames_per_buffer=CHUNK)
     
-    # Create stream
-    stream = audio.open(format=FORMAT,
-            channels=CHANNELS,
-            rate=RATE,
-            input=True,
-            output=True,
-            frames_per_buffer=CHUNK)
+        # Setup outlet stream infos
+        self.stream_info_audio = StreamInfo('Audio', 'Experimental', CHUNK, RATE/CHUNK,
+                                       'float32', 'audioid_1')
+        
+        self.streaming = False
+        
+        
+    def start():
+        # Create outlets
+        outlet_audio = StreamOutlet(stream_info_audio)
+        self.streaming = True
+        
+        self.stream_thread = threading.Thread(target=self.stream)
+        self.stream_thread.start()
     
-    # Setup outlet stream infos
-    stream_info_audio = StreamInfo('Audio', 'Experimental', CHUNK, RATE/CHUNK,
-                                   'float32', 'audioid_1')
-    
-    # Create outlets
-    outlet_audio = StreamOutlet(stream_info_audio)
-    
-    while True:
-        data = stream.read(CHUNK)
-        decoded = np.fromstring(data, 'Float32')
-        outlet_audio.push_sample(decoded)
+    def stream():
+        print("Microphone stream opened")
+        while self.streaming:
+            data = self.stream.read(self.CHUNK)
+            decoded = np.fromstring(data, 'Float32')
+            outlet_audio.push_sample(decoded)
+        print("Microphone stream closed")
 
+    def stop():
+        self.streaming = False
