2c4135ce9327fca724fc3c105988d21474a42bc7
adonunes <adonay.s.nunes@gmail.com>
2021-03-16 16:42:41 -0400

    LSL mouse stream class

--- iout/mouse_tracker.py
+++ iout/mouse_tracker.py
@@ -2,15 +2,34 @@ from pynput import mouse
 from pylsl import StreamInfo, StreamOutlet
 
 
-def mouse_stream():
-    task = 1.0
+
+class MouseStream():
+    def __init__(self):
+        
+        info = StreamInfo(name='Mouse', type='mouse', channel_count=2, 
+                          channel_format='int32',source_id='myuid34234')
+        
+        self.outlet = StreamOutlet(info)
     
-    def on_move(x, y):
-        mysample = [x,y,task]
-        outlet.push_sample(mysample)
+        self.streaming = False
+        
+        
+    def start(self):
+        self.streaming = True        
+        self.stream()
+        self.listener.start()
+        
+        
+    def stream(self):
+        
+        def on_move(x, y):
+            mysample = [x,y]
+            self.outlet.push_sample(mysample)    
+            
+        self.listener = mouse.Listener(on_move=on_move)
+        
+                       
+    def stop(self):
+        self.streaming = False        
+        self.listener.stop()
     
-    info = StreamInfo(name='Mouse', type='mouse', channel_count=3, channel_format='float32',source_id='myuid34234')
-    outlet = StreamOutlet(info)
-    
-    with mouse.Listener(on_move=on_move) as listener:
-        listener.join()
