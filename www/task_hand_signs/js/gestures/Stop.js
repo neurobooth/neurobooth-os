a75582e9ff03ccf3e28c4484a2bb37df30f2c08c
Adonay Nunes <adonay.s.nunes@gmail.com>
2021-03-25 09:59:03 -0400

    Retro updates msi laptop

--- www/task_hand_signs/js/gestures/Stop.js
+++ www/task_hand_signs/js/gestures/Stop.js
@@ -11,6 +11,7 @@ stopDescription.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0);
 
 // index:
 stopDescription.addCurl(Finger.Index, FingerCurl.NoCurl, 1.0);
+stopDescription.addDirection(Finger.Index, FingerDirection.VerticalUp, 1.0);
 
 // middle:
 stopDescription.addCurl(Finger.Middle, FingerCurl.NoCurl, 1.0);
@@ -23,5 +24,8 @@ stopDescription.addCurl(Finger.Ring, FingerCurl.NoCurl, 1.0);
 stopDescription.addCurl(Finger.Pinky, FingerCurl.NoCurl, 1.0);
 
 
+// give additional weight to index and ring fingers
+//stopDescription.setWeight(Finger.Index, 2);
+//stopDescription.setWeight(Finger.Thumb, 2);
 
 export default stopDescription;
