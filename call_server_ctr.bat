4d366d17f1bff5979e524d2047801cd048bac2db
adonunes <adonay.s.nunes@gmail.com>
2021-04-28 10:57:21 -0400

    Call schedule acq

--- call_server_ctr.bat
+++ call_server_ctr.bat
@@ -1,3 +1,4 @@
-call SCHTASKS /Create /TN TaskOnEvent /TR C:\Users\neurobooth\Desktop\neurobooth\software\neurobooth-eel\server_ctr.bat /SC ONEVENT /EC Application /MO *[System/EventID=777] /f
-call SCHTASKS /Run /TN "TaskOnEvent"	
+call SCHTASKS /S stm /U stm\ACQ /P 5519 /Create /TN TaskOnEvent /TR C:\neurobooth\neurobooth-eel\server_acq.bat /SC ONEVENT /EC Application /MO *[System/EventID=777] /f
+call SCHTASKS /S stm /U stm\ACQ /P 5519 /Run /TN "TaskOnEvent"
+
 pause
