eecab82ca33816e276461691d735245fc7aec12b
adonunes <adonay.s.nunes@gmail.com>
2021-08-12 10:08:37 -0400

    Cleans batch paths change

--- neurobooth_os/call_server_acq.bat
+++
@@ -1,4 +0,0 @@
-call SCHTASKS /S acq /U acq\ACQ /P 5519 /Create /TN TaskOnEvent /TR C:\neurobooth-eel\serv_acq.bat /SC ONEVENT /EC Application /MO *[System/EventID=777] /f
-call SCHTASKS /S acq /U acq\ACQ /P 5519 /Run /TN "TaskOnEvent"
-START /B /wait robocopy  /MOV  C:\neurobooth\neurobooth_data Z:\session_data
-pause
--- neurobooth_os/call_server_stm.bat
+++
@@ -1,4 +0,0 @@
-call SCHTASKS /S stm /U stm\STM /P 5519 /Create /TN TaskOnEvent /TR C:\neurobooth-eel\server_stm.bat /SC ONEVENT /EC Application /MO *[System/EventID=777] /f
-call SCHTASKS /S stm /U stm\STM /P 5519 /Run /TN "TaskOnEvent"	
-START /B /wait robocopy  /MOV  C:\neurobooth\neurobooth_data Z:\session_data
-pause
