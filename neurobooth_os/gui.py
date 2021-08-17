59b30d1ee71ad856ccec7685be4019fe54748220
adonunes <adonay.s.nunes@gmail.com>
2021-08-17 12:08:54 -0400

    ENH sess log ctr

--- neurobooth_os/gui.py
+++ neurobooth_os/gui.py
@@ -15,15 +15,16 @@ import sys
 import threading
 import matplotlib
 import queue
+from collections import OrderedDict
+from datetime import datetime
 
 import neurobooth_os.main_control_rec as ctr_rec
 from neurobooth_os.realtime.lsl_plotter import update_streams_fromID, get_lsl_images, stream_plotter
 from neurobooth_os.netcomm.server_ctr import server_com
 from neurobooth_os.netcomm.client import socket_message
 from neurobooth_os.layouts import main_layout, win_gen, init_layout
+import neurobooth_os.iout.metadator as meta
 
-import iout.metadator as meta
-    
 def get_session_info(values):
      session_info = values        
      tasks = get_tasks(values)
@@ -42,10 +43,21 @@ def get_tasks(values):
             tasks.append(key)
     return tasks            
 
+def task_to_obs(task, obs_list):
+    #match task to obs assuming taskname_task_n, obsname_obs taskname == obsname_obs
+    for obs in obs_list:
+        if task.split("_")[0] in obs:
+            obs_id = obs
+            return obs_id
+        
+        
 def get_outlet_ids(str_prt, dic_ids):
     # Get outlet ids from ctr server, string format = Srv:-OUTLETID-:name:uuid "
     if str_prt.split(":")[0] == "-OUTLETID-":
-        dic_ids[str_prt.split(":")[1]] = str_prt.split(":")[2]
+        try:
+            dic_ids[str_prt.split(":")[1]] = str_prt.split(":")[2]
+        except:
+            print(f"Outlet id bad formatted: {str_prt}")
     return dic_ids
     
 serv_event = queue.Queue(maxsize=10)
@@ -56,24 +68,17 @@ def feedback_com():
                
     server_com(callback)
 
-
 thr = threading.Thread(target=feedback_com, daemon=True)
 thr.start()
-        
-
-
-lsl_version = pylsl.__version__
 
 window = win_gen(init_layout)      
 
 plttr = stream_plotter()
 
-
 inlets, stream_ids = {}, {}
-plot_elem = []
+plot_elem, done_tasks, inlet_keys = [], [], []
 running_task, start_tasks, session_saved = None, False, False
 dev_prepared = False
-done_tasks = []
 
 
 def serv_data_received():
@@ -98,22 +103,36 @@ def serv_data_received():
                     inlets = update_streams_fromID(stream_ids)
             if "UPDATOR:" in event_feedb:
                 #UPDATOR:-chars-
-                rout= re.search("UPDATOR:-([a-z_]*)-", event_feedb)
+                rout= re.search("UPDATOR:-([A-Za-z_]*)-", event_feedb)
                 for expr in rout.groups():                    
                     window.write_event_value('-update_butt-', expr)
-                
-                
-                
+            
         except queue.Empty:
             break
 
-event, values = window.read(.5) 
-
 
 conn = meta.get_conn()
-statecolor_init_serv = ["green", "yellow"]
+log_id = meta.make_new_tech_obs_id(conn)
+   
+tech_obs_log = OrderedDict()
+tech_obs_log["tech_obs_log_id"] = ""
+tech_obs_log["subject_id"] = ""
+tech_obs_log["study_id"] = ""
+tech_obs_log["tech_obs_id"] = ""
+tech_obs_log["staff_id"] = ""
+tech_obs_log["application_id"] = "neurobooth_os"
+tech_obs_log["site_date"] = datetime.now().strftime("%Y_%m_%d")
+tech_obs_log["event_array"] =[]
+tech_obs_log["collection_name"] = ""
+tech_obs_log["sensor_file_ids"] = ""
+tech_obs_log["date_times"] = datetime.now().strftime("%Y_%m_%d")
 
-inlet_keys = []
+
+statecolors = {"init_servs" : ["green", "yellow"],
+               "Connect" :  ["green", "yellow"],
+               }
+
+event, values = window.read(.1) 
 while True:
     event, values = window.read(.5)
     serv_data_received()
@@ -123,22 +142,20 @@ while True:
     
     elif event == "study_id":
         print(event, values)
-        study_id = values[event]
-        
-        collection_ids = meta.get_collection_ids(study_id, conn)
-        
+        study_id = values[event]   
+        tech_obs_log["study_id"] = study_id
+        collection_ids = meta.get_collection_ids(study_id, conn)        
         window["collection_id"].update(values=collection_ids)
         
     elif event == "collection_id":
         print(event, values)
         collection_id = values[event]
-
+        tech_obs_log["collection_id"] = collection_id
         tasks_obs = meta.get_tasks(collection_id, conn)
         task_list = []
         for task in tasks_obs:
             task_id, _, _ = meta.get_task_param(task, conn)
-            task_list.append(task_id)
-        
+            task_list.append(task_id)        
         window["_tasks_"].update(value=", ".join(task_list))
                 
     elif event == "_init_sess_save_":
@@ -149,21 +166,28 @@ while True:
                 del values[0]
             sess_info = values
             subj_id = sess_info['subj_id']
-            staff_id = sess_info['staff_id']
-            
-            window.close()            
+            staff_id = sess_info['staff_id']   
+            tech_obs_log["staff_id"] = staff_id
+            tech_obs_log["subj_id"] = subj_id
+            window.close()
+            # Open new layout with main window            
             window = win_gen(main_layout, sess_info)
             
     elif event == "-update_butt-":
-        if values['-update_butt-'] == 'init_servs':
-            # 2 colors for init_servers, 1 connected, 2 connected
-            if len(statecolor_init_serv):
-                color = statecolor_init_serv.pop()
+        if values['-update_butt-'] in list(statecolors):
+            # 2 colors for init_servers and Connect, 1 connected, 2 connected
+            if len(statecolors[values['-update_butt-']]):
+                color = statecolors[values['-update_butt-']].pop()
                 window[values['-update_butt-']].Update(button_color=('black', color))
             continue
-        
         window[values['-update_butt-']].Update(button_color=('black', 'green'))
-    
+        
+    elif event == "init_servs":        
+        window['init_servs'].Update(button_color=('black', 'red'))
+        event, values = window.read(.1)
+        ctr_rec.start_servers()
+        _ = ctr_rec.test_lan_delay(50)
+        
     elif event == 'RTD':
         ctr_rec.prepare_feedback()
         print('RTD')
@@ -195,50 +219,27 @@ while True:
         else:
             plttr.start(inlets)
         
-    elif event == 'Save':
-        session_info, tasks = get_session_info(values)
-        sess_info['subj_id'] = sess_info['subj_id']
-        sess_info['staff_id'] = sess_info['staff_id']
-        sess_info['study_id'] = sess_info['study_id']
-        session_saved = True
-        print(values)
-        
     elif event == 'Test_network':
         _ = ctr_rec.test_lan_delay(50)
-        
-    elif event == "init_servs":
-        
-        window['init_servs'].Update(button_color=('black', 'red'))
-        
-        ctr_rec.start_servers()
-        _ = ctr_rec.test_lan_delay(50)
-    
-    elif event == 'eyetracker':
-        print(event)
-        socket_message(event, "presentation")
-        print(event, "finished")
-        
-    elif event == 'Start':
-        if not session_saved:
-            session_info, tasks = get_session_info(values)
-            session_info['subj_id'] = sess_info['subj_id']
-            session_info['staff_id'] = sess_info['staff_id']
-            session_info['study_id'] = sess_info['study_id']
-            print(values)
+                  
+    elif event == 'Start':        
+        session_info, tasks = get_session_info(values)
+        session_info['subj_id'] = sess_info['subj_id']
+        session_info['staff_id'] = sess_info['staff_id']
+        session_info['study_id'] = sess_info['study_id']
+        print(values)
         
         inlets = update_streams_fromID(stream_ids)
         time.sleep(.5)
         serv_data_received()
         
         if len(tasks):
-            start_tasks= True
-            
+            start_tasks= True            
             running_task = "-".join(tasks)  # task_name can be list of task1-task2-task3                       
             ctr_rec.task_presentation(running_task, session_info['subj_id'])
         else:
             print("Start button pressed but no task selected")      
-        
-       
+     
     elif event ==  'Shut Down':
         for k in list(inlets.keys()):
             if k in inlets.keys():
@@ -256,6 +257,7 @@ while True:
     
     serv_data_received()
     
+    # Get LSL inlets
     for k in stream_ids.keys():
         if k not in inlet_keys:
             inlet_keys.append(k)            
