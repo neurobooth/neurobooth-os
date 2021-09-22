9b6f1e08a3bb4173cc95c0143cd76ed616b4ed0f
Mainak Jas <mainakjas@gmail.com>
2021-09-22 15:58:40 -0400

    More

--- neurobooth_os/tests/test_neurobooth.py
+++ neurobooth_os/tests/test_neurobooth.py
@@ -8,40 +8,43 @@ table_dfs = dict()
 column_names = dict()
 dtypes = dict()
 
+ssh_username = 'mj513'
+
 def create_mock_database():
-    pass
+
+    # Create an SSH tunnel
+    with SSHTunnelForwarder(
+        'neurodoor.nmr.mgh.harvard.edu',
+        ssh_username=ssh_username,
+        ssh_config_file='~/.ssh/config',
+        ssh_pkey='~/.ssh/id_rsa',
+        remote_bind_address=('192.168.100.1', 5432),
+        local_bind_address=('localhost', 6543)) as tunnel:
+
+        with psycopg2.connect(database='neurobooth', user='neuroboother',
+                            password='neuroboothrocks', host=tunnel.local_bind_host,
+                            port=tunnel.local_bind_port) as conn:
+
+            table_ids = list_tables(conn)
+            for table_id in table_ids:
+                print(f'Table: {table_id}')
+                table = Table(table_id, conn)
+                table_dfs[table_id] = table.query(f'SELECT * from {table_id}')
+                column_names[table_id] = table.column_names
+                dtypes[table_id] = table.dtypes
+
+        with psycopg2.connect(database='mock_neurobooth', user='neuroboother',
+                            password='neuroboothrocks', host=tunnel.local_bind_host,
+                            port=tunnel.local_bind_port) as conn_mock:
+            for table_id in table_ids:
+                create_table(table_id, conn, column_names[table_id],
+                            dtypes[table_id])
+
 
 def delete_mock_database():
     pass
 
-# Create an SSH tunnel
-with SSHTunnelForwarder(
-    'neurodoor.nmr.mgh.harvard.edu',
-    ssh_username='an512',
-    ssh_config_file='~/.ssh/config',
-    ssh_pkey='~/.ssh/id_rsa',
-    remote_bind_address=('192.168.100.1', 5432),
-    local_bind_address=('localhost', 6543)) as tunnel:
-
-    with psycopg2.connect(database='neurobooth', user='neuroboother',
-                          password='neuroboothrocks', host=tunnel.local_bind_host,
-                          port=tunnel.local_bind_port) as conn:
-
-        table_ids = list_tables(conn)
-        for table_id in table_ids:
-            print(f'Table: {table_id}')
-            table = Table(table_id, conn)
-            table_dfs[table_id] = table.query(f'SELECT * from {table_id}')
-            column_names[table_id] = table.column_names
-            dtypes[table_id] = table.dtypes
-
-    with psycopg2.connect(database='mock_neurobooth', user='neuroboother',
-                          password='neuroboothrocks', host=tunnel.local_bind_host,
-                          port=tunnel.local_bind_port) as conn_mock:
-        for table_id in table_ids:
-            create_table(table_id, conn, column_names[table_id],
-                         dtypes[table_id])
-
 def test_neurobooth():
     """Call function to test neurobooth."""
-    pass
+    create_mock_database()
+
