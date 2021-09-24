7b8c868b15e750f461bb966a0f8842d60ebc426b
Mainak Jas <mainakjas@gmail.com>
2021-09-24 15:40:43 -0400

    ENH: Mock database works

--- neurobooth_os/tests/connect_to_neurodoor.py
+++ neurobooth_os/tests/connect_to_neurodoor.py
@@ -34,14 +34,22 @@ def create_mock_database():
             table.insert_rows([('mock_study', 0, 'mock_study', 0, 0,
                                 '{mock_collection}', None, None)])
 
-            table = Table('collections', conn_mock)
+            table = Table('collection', conn_mock)
             table.insert_rows([('mock_collection', False, '{mock_obs}', None)])
+
+            table = Table('stimulus', conn_mock)
+            table.insert_rows([('mock_test_1', 'description', 2, None,
+                                'stream_python',
+                                'tasks.test.mock_test.py::mock_stim()',
+                                None, None)])
 
             table = Table('tech_obs_data', conn_mock)
             table.insert_rows([('mock_obs', None, 'testing', None, None, None,
-                                'mock_test_1', '{mock_dev_1, mock_mbient_1, mock_Intel_1}')])
+                                'mock_test_1', '{mock_dev_1, mock_mbient_1, mock_Intel_1}',
+                                None, None, None, None,
+                                '{{mock_sens_1, ""},{mock_mbient_acc_1, mock_mbient_grad_1},{mock_Intel_rgb_1, mock_Intel_depth_1}}')])
 
-            table = Table('devices')
+            table = Table('device', conn_mock)
             table.insert_rows([('mock_dev_1', 0, False, 0, 'mock', 'mock_make',
                                 'neurobooth inc', 0, '{mock_sens_1}')])
             table.insert_rows([('mock_mbient_1', 0, False, 0, 'mock',
@@ -58,12 +66,6 @@ def create_mock_database():
             table.insert_rows([('mock_Intel_rgb_1', 180, 1080, 720, 'bag', None)])
             table.insert_rows([('mock_Intel_depth_1', 180, 1080, 720, 'bag', None)])
 
-            table = Table('stimulus', conn_mock)
-            table.insert_rows([('mock_test_1', 'description', 2, None,
-                                'stream_python',
-                                'tasks.test.mock_test.py::mock_stim()',
-                                None, None)])
-
 
 def delete_mock_rows():
     """Delete mock."""
@@ -78,13 +80,13 @@ def delete_mock_rows():
         with psycopg2.connect(database='mock_neurobooth', user='neuroboother',
                               password='neuroboothrocks', host=tunnel.local_bind_host,
                               port=tunnel.local_bind_port) as conn_mock:
-            table_ids = ['study', 'collections', 'tech_obs_data', 'devices',
-                         'sensors', 'stimulus']
+            table_ids = ['study', 'collection', 'tech_obs_data', 'device',
+                         'sensor', 'stimulus']
             primary_keys = ['study_id', 'collection_id', 'tech_obs_id',
                             'device_id', 'sensor_id', 'stimulus_id']
             for table_id, pk in zip(table_ids, primary_keys):
                 table = Table(table_id, conn_mock)
-                table.delete_row(f'{pk} LIKE mock\%')
+                table.delete_row(f"{pk} LIKE 'mock%'")
 
 
 def test_neurobooth():
