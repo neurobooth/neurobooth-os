360ce2531f6fb0bf9c72b91e06abb40246b1f0be
Adonay Nunes <adonay.s.nunes@gmail.com>
2021-09-25 13:05:54 -0400

    MINT gui steps

--- neurobooth_os/gui.py
+++ neurobooth_os/gui.py
@@ -6,14 +6,9 @@ Created on Fri Apr  2 08:01:51 2021
 """
 
 import time
-import re
-import sys
 import threading
-import queue
 from collections import OrderedDict
 from datetime import datetime
-
-import numpy as np
 
 import PySimpleGUI as sg
 
@@ -24,28 +19,12 @@ from neurobooth_os.layouts import _main_layout, _win_gen, _init_layout
 import neurobooth_os.iout.metadator as meta
 
 
-def _get_tasks(values):    
-    tasks = []
-    for key, val in values.items():
-        if "task" in key and val is True:
-            tasks.append(key)
-    return tasks
-
-
-serv_event = queue.Queue(maxsize=100)
-thr = threading.Thread(target=get_messages_to_ctr, args=(serv_event,), daemon=True)
-thr.start()
-
 
 REMOTE=False, 
 DATABASE='neurobooth'
 
 conn = meta.get_conn(remote=REMOTE, database=DATABASE)
-
 window = _win_gen(_init_layout, conn)
-
-
-plttr = stream_plotter()
 
 
 def new_tech_log_dict(application_id="neurobooth_os"):
@@ -62,60 +41,53 @@ def new_tech_log_dict(application_id="neurobooth_os"):
     return tech_obs_log
 
 
-inlets, stream_ids = {}, {}
-def serv_data_received():
-    # process CTR server received data
-    global stream_ids, inlets, window
 
-    while True:  # while items in queue
-        try:
-            event_feedb = serv_event.get(False)
+def process_received_data(serv_data, window):
+    """ Gets string data from other servers and create PySimpleGui window events.
 
-            stream_ids_old = stream_ids.copy()
-            # remove server name
-            serv_name = event_feedb.split(": ")[0]
-            event_feedb = event_feedb.replace("STM: ", "").replace("ACQ: ", "")
-            print(serv_name, ":::")
-            print("event_row " , event_row)
+    Parameters
+    ----------
+    serv_data : str
+        Data sent by other servers
+    window : object
+        PySimpleGui window object
+    """
 
-            for event_row in event_feedb.split("\n"):
+    # Split server name and data
+    serv_name, serv_data = serv_data.split(":::")
+    print(serv_name, ":::")
+    print("serv_data " , serv_data)
 
-                if "-OUTLETID-" in event_row:
-                    # -OUTLETID-:OutletName:uuid
-                    id_inf = re.search(
-                        "-OUTLETID-:([A-Za-z_0-9]*):([A-Za-z_0-9]*)", event_row)
-                    id_name, id_uuid = id_inf.groups()
-                    stream_ids[id_name] = id_uuid             
-                    if stream_ids_old != stream_ids or (len(stream_ids) and len(inlets) == 0):
-                        inlets = update_streams_fromID(stream_ids)
+    for data_row in serv_data.split("\n"):
+        if "-OUTLETID-" in data_row:
+            # -OUTLETID-:outlet_name:uuid
+            evnt, outlet_name, outlet_id = data_row.split(":")
+            window.write_event_value('-OUTLETID-', f"['{outlet_name}', '{outlet_id}']")
 
-                elif "UPDATOR:" in event_row:
-                    # UPDATOR:-chars-
-                    rout = re.search("UPDATOR:-([A-Za-z_]*)-", event_row)
-                    for expr in rout.groups():
-                        window.write_event_value('-update_butt-', expr)
+        elif "UPDATOR:" in data_row:
+            # UPDATOR:-elem_key-
+            elem = data_row.split(":")[1]
+            window.write_event_value('-update_butt-', elem)
 
-                elif "Initiating task:" in event_row:
-                    # Initiating task:task_id:obs_id
-                    task_inf = re.search(
-                        "Initiating task: ([A-Za-z_0-9]*):([A-Za-z_0-9]*)", event_row)
-                    task_id, obs_id = task_inf.groups()
-                    window["task_title"].update("Running Task:")
-                    window["task_running"].update(task_id, background_color="red")
-                    window['Start'].Update(button_color=('black', 'red'))
+        elif "Initiating task:" in data_row:
+            # Initiating task:task_id:obs_id
+            _, task_id, obs_id = data_row.split(":")
+            window.write_event_value('task_initiated', f"['{task_id}', '{obs_id}']")
 
-                elif "Finished task:" in event_row:
-                    # Finished task:task_id
-                    task_inf = re.search("Finished task: ([A-Za-z_0-9]*)", event_row)
-                    task_id = task_inf.groups()
-                    window["task_running"].update(task_id, background_color="green")
-                    window['Start'].Update(button_color=('black', 'green'))
-
-        except queue.Empty:
-            break
+        elif "Finished task:" in data_row:
+            # Finished task: task_id
+            _, task_id = data_row.split(":")
+            window.write_event_value('task_finished', task_id)
 
 
+ctr_thr = threading.Thread(target=get_messages_to_ctr, args=(process_received_data, window,), daemon=True)
+ctr_thr.start()
+
+
+
+plttr = stream_plotter()
 tech_obs_log = new_tech_log_dict()
+stream_ids = {}
 plot_elem, inlet_keys = [], [],
 statecolors = {"init_servs": ["green", "yellow"],
                "Connect": ["green", "yellow"],
@@ -124,8 +96,6 @@ statecolors = {"init_servs": ["green", "yellow"],
 event, values = window.read(.1)
 while True:
     event, values = window.read(.5)
-
-    serv_data_received()
     
     if event == sg.WIN_CLOSED:
         break
@@ -178,11 +148,10 @@ while True:
         ctr_rec.start_servers()
         _ = ctr_rec.test_lan_delay(50)
 
-    elif event == 'RTD':
+    elif event == 'RTD':  # TODO signal when RTD finishes
         ctr_rec.prepare_feedback()
         print('RTD')
         time.sleep(1)
-        serv_data_received()
 
     elif event == 'Connect':
         window['Connect'].Update(button_color=('black', 'red'))
@@ -191,18 +160,23 @@ while True:
         ctr_rec.prepare_devices(f"{collection_id}:{str(tech_obs_log)}")
         ctr_rec.initiate_labRec()
         print('Connecting devices')
-        serv_data_received()
-        inlets = update_streams_fromID(stream_ids)
+
+    elif event == "-OUTLETID-":
+        # event values -> f"['{outlet_name}', '{outlet_id}']
+        outlet_name, outlet_id = eval(values[event])
+
+        # update inlet if new or outlet_id new != old   
+        if stream_ids.get(outlet_name) is None or outlet_id != stream_ids[outlet_name]:
+            stream_ids[outlet_name] = outlet_id
+            inlets = update_streams_fromID(stream_ids)  # TODO update only new outid
 
     elif event == 'plot':
-        event = "None"
+        # if no inlets sent event to prepare devices and make popup error
         if len(inlets) == 0:
-            ctr_rec.prepare_devices(collection_id)
-            serv_data_received()
-            inlets = update_streams_fromID(stream_ids)
+            window.write_event_value('Connect')
+            sg.PopupError('No inlet devices detected, preparing. Press plot once prepared')
 
         if plttr.pltotting_ts is True:
-            inlets = update_streams_fromID(stream_ids)
             plttr.inlets = inlets
         else:
             plttr.start(inlets)
@@ -211,35 +185,38 @@ while True:
         _ = ctr_rec.test_lan_delay(50)
 
     elif event == 'Start':
-        tasks = _get_tasks(values)
-
-        inlets = update_streams_fromID(stream_ids)
-        time.sleep(.5)
-        
-        serv_data_received()
+        tasks = [k for k, v in values.items() if "task" in k and v == True]
+                
         window['Start'].Update(button_color=('black', 'yellow'))
         if len(tasks):
-            start_tasks = True
             running_task = "-".join(tasks)  # task_name can be list of task1-task2-task3
             ctr_rec.task_presentation(running_task, sess_info['subj_id'])
         else:
             sg.PopupError('No task selected')
 
+    elif event == 'task_initiated':
+        # event values -> f"['{task_id}', '{obs_id}']
+        task_id, obs_id = eval(values[event])
+
+        window["task_title"].update("Running Task:")
+        window["task_running"].update(task_id, background_color="red")
+        window['Start'].Update(button_color=('black', 'red'))
+
+    elif event == 'task_finished':
+        task_id = values[event]
+        window["task_running"].update(task_id, background_color="green")
+        window['Start'].Update(button_color=('black', 'green'))
+
     elif event == 'Shut Down':
-        for k in list(inlets.keys()):
-            if k in inlets.keys():
-                inlets[k].close_stream()
-                inlets.pop(k, None)
-        ctr_rec.shut_all()
         plttr.stop()
+        ctr_rec.shut_all()
         break
 
+    # Plotting STM screen or webcam
     if any(k for k in inlets.keys() if k in ["Webcam", "Screen"]):
         plot_elem = get_lsl_images(inlets)
         for elem in plot_elem:
             window[elem[0]].update(data=elem[1])
-
-    serv_data_received()
 
     # Display LSL inlets
     if inlet_keys != list(stream_ids):
@@ -249,6 +226,5 @@ while True:
 
 
 window.close()
-# window['-OUTPUT-'].__del__()
-serv_data_received()
+window['-OUTPUT-'].__del__()
 print("Session terminated")
