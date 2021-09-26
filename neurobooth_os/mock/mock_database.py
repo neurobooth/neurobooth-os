0a3841e15850f2811ccf0deba49df24b21f2dde6
Adonay Nunes <adonay.s.nunes@gmail.com>
2021-09-26 11:17:00 -0400

    ADD read mock dev param

--- neurobooth_os/mock/mock_database.py
+++ neurobooth_os/mock/mock_database.py
@@ -31,34 +31,41 @@ def insert_mock_rows(conn_mock):
    # parameters_dict =add a parameter json  
     table = Table('stimulus', conn_mock)
     table.insert_rows([('mock_test_1', 'description', 2, 5,                        
-                        'mock.mock_task.py::simulated_task()', parameters_dict)],
+                        'mock.mock_task.py::simulated_task()')],
                         cols=['stimulus_id', 'stimulus_description', 'num_iterations', "duration",
-                         'stimulus_file', 'parameters'])
+                         'stimulus_file'])
 
     # create mock inst row
-   # instruction_id = Table('instruction', conn_mock).insert_rows([(7,)], cols=['instruction_id'])
+    instruction_id = Table('instruction', conn_mock).insert_rows(
+        ["this is a mock task, follow the instructions"], cols=['instruction_text'])
 
     table = Table('tech_obs_data', conn_mock)
     table.insert_rows([('mock_obs', None, 'testing', None, instruction_id, None,
                         'mock_test_1',
-                        '{mock_dev_1, mock_mbient_1, mock_Intel_1}',
+                        '{mock_dev_1, mock_Mbient_1,mock_Mbient_2, mock_Intel_1}',
                         None, None, None, None,
-                        '{{mock_sens_1, ""},{mock_mbient_acc_1, mock_mbient_grad_1},{mock_Intel_rgb_1, mock_Intel_depth_1}}')])
+                        ('{{mock_sens_1, ""},{mock_Mbient_acc_1, mock_Mbient_grad_1},'
+                         '{mock_Mbient_acc_1, mock_Mbient_grad_1},'
+                         '{mock_Intel_rgb_1, mock_Intel_depth_1}}')
+                         )])
 
     table = Table('device', conn_mock)
     table.insert_rows([('mock_dev_1', 0, False, 0, 'mock', 'mock_make',
                         'neurobooth inc', 0, '{mock_sens_1}')])
-    table.insert_rows([('mock_mbient_1', 0, False, 0, 'mock',
+    table.insert_rows([('mock_Mbient_1', 0, False, 0, 'mock',
                         'mock_make', 'neurobooth inc', 0,
-                        '{mock_mbient_acc_1, mock_mbient_grad_1}')])
+                        '{mock_Mbient_acc_1, mock_Mbient_grad_1}')])
+    table.insert_rows([('mock_Mbient_2', 0, False, 0, 'mock',
+                        'mock_make', 'neurobooth inc', 0,
+                        '{mock_Mbient_acc_1, mock_Mbient_grad_1}')])
     table.insert_rows([('mock_Intel_1', 0, False, 0, 'mock',
                         'mock_make', 'neurobooth inc', 0,
                         '{mock_Intel_rgb_1, mock_Intel_depth_1}')])
 
     table = Table('sensor', conn_mock)
     table.insert_rows([('mock_sens_1', 100, None, None, 'edf', None)])
-    table.insert_rows([('mock_mbient_acc_1', 100, None, None, 'edf', None)])
-    table.insert_rows([('mock_mbient_grad_1', 100, None, None, 'edf', None)])
+    table.insert_rows([('mock_Mbient_acc_1', 100, None, None, 'edf', None)])
+    table.insert_rows([('mock_Mbient_grad_1', 100, None, None, 'edf', None)])
     table.insert_rows([('mock_Intel_rgb_1', 180, 1080, 720, 'bag', None)])
     table.insert_rows([('mock_Intel_depth_1', 180, 1080, 720, 'bag', None)])
 
@@ -72,9 +79,9 @@ def delete_mock_rows(conn_mock):
         The connection object to the mock database.
     """
     table_ids = ['study', 'collection', 'tech_obs_data', 'device',
-                    'sensor', 'stimulus']
+                    'sensor', 'stimulus', 'instruction']
     primary_keys = ['study_id', 'collection_id', 'tech_obs_id',
-                    'device_id', 'sensor_id', 'stimulus_id']
+                    'device_id', 'sensor_id', 'stimulus_id', 'instruction_id']
     for table_id, pk in zip(table_ids, primary_keys):
         table = Table(table_id, conn_mock)
         table.delete_row(f"{pk} LIKE 'mock%'")
