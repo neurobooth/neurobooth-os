ba3af5c0fd78a579701a8bfc84e787cd3937a491
Adonay Nunes <adonay.s.nunes@gmail.com>
2021-10-11 14:25:30 -0400

    ADD write tech obs log to DB

--- neurobooth_os/iout/tests/test_metadator.py
+++ neurobooth_os/iout/tests/test_metadator.py
@@ -1,6 +1,5 @@
 
-from datetime import datetime 
-from collections import OrderedDict
+
 from neurobooth_os.iout import metadator as meta
 
 
@@ -9,22 +8,18 @@ def test_tech_obs_addition():
 
     conn = meta.get_conn(remote=True,database="mock_neurobooth")
     subj_id = "Test"
-    tech_obs_id = meta.make_new_tech_obs_row(conn, subj_id)
+    tech_obs_id = meta._make_new_tech_obs_row(conn, subj_id)
 
-    conn = meta.get_conn(remote=True,database="mock_neurobooth")
-    vals_dict = OrderedDict()
+    vals_dict = meta._new_tech_log_dict()
     vals_dict['subject_id'] = subj_id
-    vals_dict['study_id'] = "test_study"
-    vals_dict['tech_obs_id'] = "mock_obs"
-    vals_dict['staff_id'] = "mocker"
-    vals_dict['application_id'] = "test"
-    vals_dict['event_array'] = "event:str"
+    vals_dict['study_id'] = "mock_study"
+    vals_dict['tech_obs_id'] = "mock_obs_1"
+    vals_dict['staff_id'] = "mocker"    
+    vals_dict['event_array'] = "event:datestamp"
     vals_dict['collection_id'] = "mock_collection"
-    datetime_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    vals_dict['date_times'] = "{" + datetime_str + "}"
-    vals_dict['site_id'] = "mock"
+    vals_dict['site_id'] = "mock_site"
 
-    vals = list(vals_dict.values())
-    meta.fill_tech_obs_row(tech_obs_id, tuple(vals), conn)
+    meta._fill_tech_obs_row(tech_obs_id, vals_dict, conn)
+    
 
 
