394a43944072d990285759d6cb43aed002e99297
Adonay Nunes <adonay.s.nunes@gmail.com>
2021-10-12 17:35:27 -0400

    Update realtime plotter

--- neurobooth_os/realtime/lsl_plotter.py
+++ neurobooth_os/realtime/lsl_plotter.py
@@ -88,168 +88,95 @@ def get_lsl_images(inlets, frame_sz=(320, 240)):
 
 class stream_plotter():
 
-    def __init__(self, plt_img=False, plt_ts=True):
-
+    def __init__(self):
         self.inlets = {}
-        self.plt_img = plt_img
-        self.plt_ts = plt_ts
-        self.pltotting_img = False
         self.pltotting_ts = False
 
     def start(self, inlets):
-        if any([self.pltotting_img, self.pltotting_ts]):
+        if  self.pltotting_ts:
             self.stop()
 
-        self.pltotting_img = self.plt_img
-        self.pltotting_ts = self.plt_ts
-
+        self.pltotting_ts = True
         self.inlets = inlets
 
-        if self.plt_img is True and any(
-                [k for k in ['Mouse', "mbient", "Audio"] if k in inlets.keys()]):
-            self.thread_img = threading.Thread(target=self.update_imgs, daemon=True)
-            self.thread_img.start()
-
-        if self.plt_ts is True and any(
-                [k for k in ['Mouse', "mbient", "Audio"] if k in inlets.keys()]):
+        if any([True for k in ['Mouse', "mbient", "Audio"] if any(k in v for v in list(inlets))]):       
+            print("starting thread update_ts")
             self.thread_ts = threading.Thread(target=self.update_ts, daemon=True)
             self.thread_ts.start()
         else:
             self.pltotting_ts = False
 
     def stop(self):
-        self.pltotting_img = False
         self.pltotting_ts = False
+        self.thread_ts.join()
         self.inlets = {}
         print("Closed plotting windows")
 
     def update_ts(self):
+        self.inlets_plt = [v for v in list(self.inlets) 
+                            if any([i in v for i in ['Mouse', "mbient", "Audio"]])]
+                            
         plt.ion()
-        fig, axs = plt.subplots(3, 1, sharex=False, figsize=(9.16, 9.93))
+        fig, axs = plt.subplots(len(self.inlets_plt), 1, sharex=False, figsize=(9.16, 9.93))        
         axs[-1].set_xticks([])
-        mngr = plt.get_current_fig_manager()
-        mngr.window.setGeometry(1015, 30, 905, 1005)
+        mngr = plt.get_current_fig_manager()        
+        mngr.window.setGeometry(1015, 30, 905, 1005)        
         fig.tight_layout()
-        fig.canvas.draw()
-        fig.show()
+        fig.canvas.draw()        
+        fig.show()        
         plt.show(block=False)
-
+        
         sampling = .1
         buff_size = 1024
         mypause(sampling)
-
+        
+        print('starting plotting loop')
         while self.pltotting_ts:
-
-            # frame_ts = []
-            for nm, inlet in self.inlets.items():
-                if nm in ['Marker', 'Markers']:
+            for ax_ith, nm in enumerate(self.inlets_plt):
+                inlet = self.inlets[nm]
+                tv, ts = inlet.pull_chunk(timeout=0.0)
+                if ts == []:
                     continue
 
-                elif nm in ['Mouse', "mbient", "Audio"]:
-                    tv, ts = inlet.pull_chunk(timeout=0.0)
+                clicks = []
+                if "Mouse" in nm:                    
+                    clicks = [[its, itv[-1]] for itv, its in zip(tv, ts)if itv[-1] != 0]
+                    tv = [itv[:-1] for itv in tv]
+                elif "mbient" in nm:
+                    tv = [[np.mean(itv[:3]), np.mean(itv[3:])] for itv in tv]
+                elif "Audio" in nm:
+                    tv = [[np.mean(itv)]for itv in tv]
+                tv, ts = np.array(tv), np.array(ts)
 
-                    if ts == []:
-                        continue
+                if not hasattr(inlet, "line"):
+                    inlet.xdata = np.array(range(buff_size))
+                    inlet.ydata = np.zeros((buff_size, tv.shape[1]))
+                    inlet.line = axs[ax_ith].plot(inlet.xdata, inlet.ydata)
 
-                    clicks = []
-                    if nm == "Mouse":
-                        ax_ith = 0
-                        clicks = [[tt, t[-1]] for t, tt in zip(tv, ts)if t[-1] != 0]
-                        tv = [t[:-1] for t in tv]
-                    elif nm == "mbient":
-                        ax_ith = 1
-                        tv = [[np.mean(t[:3]), np.mean(t[3:])] for t in tv]
+                inlet.ydata = np.vstack((inlet.ydata[ts.shape[0] - buff_size:-1, :], tv))
+                inlet.xdata = np.hstack((inlet.xdata[ts.shape[0] - buff_size:-1], ts))
 
-                    elif nm == "Audio":
-                        ax_ith = 2
-                        tv = [[np.mean(t)]for t in tv]
+                for i, chn in enumerate(inlet.ydata.T):
+                    inlet.line[i].set_data(inlet.xdata, chn)
 
-                    tv = np.array(tv)
-                    ts = np.array(ts)
+                if clicks:
+                    for clk in clicks:
+                        clr = "g" if clk[1] == 1 else "r" if clk[1] == -1 else "k"
+                        axs[ax_ith].axvline(x=clk[0], color=clr)
 
-                    sz = ts.shape[0]
+                axs[ax_ith].set_xlim([max(ts) - (sampling * 50), max(ts)])
+                ylim = inlet.ydata.flatten()
+                axs[ax_ith].set_ylim([min(ylim), max(ylim)])
 
-                    if not hasattr(inlet, "line"):
-                        inlet.xdata = np.array(range(buff_size))
-                        inlet.ydata = np.zeros((buff_size, tv.shape[1]))
-
-                        inlet.line = axs[ax_ith].plot(inlet.xdata, inlet.ydata)
-
-                    inlet.ydata = np.vstack((inlet.ydata[sz - buff_size:-1, :], tv))
-                    inlet.xdata = np.hstack((inlet.xdata[sz - buff_size:-1], ts))
-
-                    for i, chn in enumerate(inlet.ydata.T):
-                        inlet.line[i].set_data(inlet.xdata, chn)
-
-                    if clicks != []:
-                        for clk in clicks:
-                            clr = "g" if clk[1] == 1 else "r" if clk[1] == -1 else "k"
-                            axs[ax_ith].axvline(x=clk[0], color=clr)
-
-                    axs[ax_ith].set_xlim([max(ts) - (sampling * 50), max(ts)])
-                    ylim = inlet.ydata.flatten()
-                    axs[ax_ith].set_ylim([min(ylim), max(ylim)])
-
-            mypause(sampling)
-            if len(self.inlets) == 0:
+            
+            if len(self.inlets_plt) == 0:
                 self.pltotting_ts = False
                 break
-            # time.sleep(.1)
+
+            mypause(sampling)
 
         self.pltotting_ts = False
         plt.close(fig)
-
-    def update_imgs(self):
-
-        frame_screen, frame_cam = np.ones(
-            (240, 320), dtype=np.uint8), np.ones(
-            (240, 320), dtype=np.uint8)
-
-        nqueue = 10
-        frame_queue_scr, frame_queue_cam = [], []
-        for n in range(nqueue):
-            frame_queue_scr.append(frame_screen)
-            frame_queue_cam.append(frame_cam)
-
-        # cv2.namedWindow("Output Frame")
-
-        while self.pltotting_img:
-            for nm, inlet in self.inlets.items():
-
-                if nm in ['Marker', 'Markers']:
-                    continue
-
-                elif nm == "Screen":
-                    tv, ts = inlet.pull_sample(timeout=0.0)
-                    if ts == [] or ts is None:
-                        continue
-
-                    tv = tv[1:]  # First element is frame number
-                    frame_screen = np.array(tv, dtype=np.uint8).reshape(240, 320)
-
-                    frame_queue_scr.pop(0)
-                    frame_queue_scr.append(frame_screen)
-
-                elif nm == "Webcam":
-                    tv, ts = inlet.pull_sample(timeout=0.0)
-                    if ts == [] or ts is None:
-                        continue
-
-                    frame_cam = np.array(tv, dtype=np.uint8).reshape(240, 320)
-
-                    frame_queue_cam.pop(0)
-                    frame_queue_cam.append(frame_cam)
-
-            frames = cv2.hconcat(frame_queue_cam), cv2.hconcat(frame_queue_scr)
-            frames = cv2.vconcat(frames)
-
-            cv2.imshow("Output Frame", frames)
-
-            key = cv2.waitKey(1) & 0xFF
-            if key == ord:
-                break
-
-        cv2.DestroyWindow("Output Frame")
 
 
 def mypause(interval):
@@ -261,13 +188,4 @@ def mypause(interval):
             if canvas.figure.stale:
                 canvas.draw()
             canvas.start_event_loop(interval)
-            # return
 
-
-if 0:
-    ppt = stream_plotter(plt_img=True, plt_ts=True)
-    ppt.start()
-
-    if 0:
-        ppt.stop()
-# ppt.update_ts()
