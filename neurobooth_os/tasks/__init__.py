62144729c1ccf448dc43e5865e1954ab836aa3b6
adonunes <adonay.s.nunes@gmail.com>
2021-10-14 11:42:12 -0400

    ADD base task

--- neurobooth_os/tasks/__init__.py
+++ neurobooth_os/tasks/__init__.py
@@ -5,5 +5,5 @@ Created on Tue Jul 20 10:24:54 2021
 @author: STM
 """
 
-from neurobooth_os.tasks.base_task import Base_Task
+from neurobooth_os.tasks.task import Task
 from neurobooth_os.tasks.s2s.task_s2s import SitToStand
