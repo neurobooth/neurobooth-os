2a73ca4890504c0db37c3d0e0828846a0fb7016a
Adonay Nunes <adonay.s.nunes@gmail.com>
2021-10-18 15:42:56 -0400

    Update files with new secrets config

--- neurobooth_os/mock/tests/test_mock_database.py
+++ neurobooth_os/mock/tests/test_mock_database.py
@@ -2,23 +2,22 @@ import psycopg2
 from sshtunnel import SSHTunnelForwarder
 
 from neurobooth_os.mock import insert_mock_rows, delete_mock_rows
-
+from neurobooth_os.secrets_info import secrets
 
 def test_neurobooth_mock():
     """Call function to test neurobooth."""
 
-    ssh_username = 'mj513'
     with SSHTunnelForwarder(
         'neurodoor.nmr.mgh.harvard.edu',
-        ssh_username=ssh_username,
+        ssh_username=secrets['database']['remote_username'],
         ssh_config_file='~/.ssh/config',
         ssh_pkey='~/.ssh/id_rsa',
         remote_bind_address=('192.168.100.1', 5432),
         local_bind_address=('localhost', 6543)) as tunnel:
 
         with psycopg2.connect(database='mock_neurobooth',
-                              user='neuroboother',
-                              password='neuroboothrocks',
+                              user=secrets['database']['user'],
+                              password=secrets['database']['pass'],                              
                               host=tunnel.local_bind_host,
                               port=tunnel.local_bind_port) as conn_mock:
             delete_mock_rows(conn_mock)
