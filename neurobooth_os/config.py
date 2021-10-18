e30b3da440250cf7d351cd47ffabd38e599edf98
Adonay Nunes <adonay.s.nunes@gmail.com>
2021-10-18 15:27:05 -0400

    secrets and config root user dir

--- neurobooth_os/config.py
+++ neurobooth_os/config.py
@@ -4,26 +4,21 @@ Created on Thu Jan 14 08:21:16 2021
 
 @author: adona
 """
+import os.path as op
+from os.path import expanduser
+import json
 
+# if files does not exist create it in user root dir
+fname = op.join(expanduser("~"), ".neurobooth_os_config")
+if not op.exists(fname):        
+    paths = {
+        "data_out": r"C:\neurobooth\neurobooth_data\\",
+        'nas': r'Z:\session_data\\',
+        'video_tasks' : r"C:\\Users\\STM\\Dropbox (Partners HealthCare)\\Neurobooth Videos for tasks\\Videos_to_present",
+        }
+    with open(fname, "w+") as f:
+        json.dump(paths, f)
 
-paths = {}
-paths["data_out"] = r"C:\neurobooth\neurobooth_data\\"
-paths["LabRecorder"] = r'C:\neurobooth\LabRecorder\LabRecorder.exe'
-paths['nas'] = r'Z:\session_data\\'
+with open(fname, 'r') as f:
+    paths = json.load(f)
 
-
-mbient_macs = {
-    "RH": "EE:99:D8:9D:69:5F",  # "D1:49:43:61:54:08",
-    "LH": "CE:F3:BD:BD:04:8F",
-    "LF": "CC:22:B5:89:7D:78"}
-
-cam_inx = {
-    "lowFeed": 0,
-    # "brio1": 4,
-    # "brio2": 7,
-    "intel1": [1, '037522250727'],
-    "intel2": [2, '036322250240'],
-    "intel3": [3, '037322251461']
-}
-
-path_vids_tasks = r"C:\Users\STM\Dropbox (Partners HealthCare)\Neurobooth Videos for tasks\Videos_to_present"
