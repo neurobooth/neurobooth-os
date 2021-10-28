374980789eeebcaba137851d33bab663e234b073
adonunes <adonay.s.nunes@gmail.com>
2021-10-28 15:25:16 -0400

    robocopy copy folders and mkdir

--- neurobooth_os/server_stm.bat
+++ neurobooth_os/server_stm.bat
@@ -1,3 +1,4 @@
 call C:\Users\STM\anaconda3\Scripts\activate.bat C:\Users\STM\anaconda3\envs\eyelink
 start /W ipython -i C:\neurobooth-eel\neurobooth_os\server_stm.py
-robocopy  /MOVE  C:\neurobooth\neurobooth_data Z:\session_data
+robocopy  /MOVE  C:\neurobooth\neurobooth_data Z:\session_data /e
+mkdir C:\neurobooth\neurobooth_data
