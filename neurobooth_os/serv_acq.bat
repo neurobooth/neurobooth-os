56502bca5cfd33e5fb04e054baac1e06430a8c94
adonunes <adonay.s.nunes@gmail.com>
2021-10-28 15:25:16 -0400

    robocopy copy folders and mkdir

--- neurobooth_os/serv_acq.bat
+++ neurobooth_os/serv_acq.bat
@@ -1,3 +1,4 @@
 call C:\Users\ACQ\anaconda3\Scripts\activate.bat C:\Users\ACQ\anaconda3\envs\neurobooth
 call start /W ipython -i C:\neurobooth-eel\neurobooth_os\server_acq.py
-robocopy  /MOVE  C:\neurobooth\neurobooth_data Z:\session_data
+robocopy  /MOVE  C:\neurobooth\neurobooth_data Z:\session_data /e
+mkdir C:\neurobooth\neurobooth_data
