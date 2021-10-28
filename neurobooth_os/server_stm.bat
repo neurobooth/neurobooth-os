3a8ed7e71925179ac76a6a5e816b6d9bc44cf754
adonunes <adonay.s.nunes@gmail.com>
2021-10-28 13:47:39 -0400

    fix bats, server_Stm

--- neurobooth_os/server_stm.bat
+++ neurobooth_os/server_stm.bat
@@ -1,3 +1,3 @@
 call C:\Users\STM\anaconda3\Scripts\activate.bat C:\Users\STM\anaconda3\envs\eyelink
-start /W python -i C:\neurobooth-eel\neurobooth_os\server_stm.py
+start /W ipython -i C:\neurobooth-eel\neurobooth_os\server_stm.py
 robocopy  /MOVE  C:\neurobooth\neurobooth_data Z:\session_data
