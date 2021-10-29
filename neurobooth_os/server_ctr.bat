7aee874f7b5e26ad6b344c236d894157ea032636
adonunes <adonay.s.nunes@gmail.com>
2021-10-29 09:35:26 -0400

    fix sleep 1 sec init serv

--- neurobooth_os/server_ctr.bat
+++ neurobooth_os/server_ctr.bat
@@ -1,4 +1,4 @@
 call  C:\Users\CTR\anaconda3\Scripts\activate.bat C:\Users\CTR\anaconda3\envs\neurobooth
-call start/W ipython -i C:\neurobooth-eel\neurobooth_os\gui.py
+call start/W ipython --pdb -i C:\neurobooth-eel\neurobooth_os\gui.py
 robocopy  /MOVE  C:\neurobooth\neurobooth_data Z:\session_data /e
 mkdir C:\neurobooth\neurobooth_data
