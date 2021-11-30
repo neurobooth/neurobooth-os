86e7275cb77a9388905adb4926eb45bba12e01e0
Adonunes <adonay.s.nunes@gmail.com>
2021-11-30 15:05:01 -0500

    Fixation base class fix

--- neurobooth_os/tasks/fixations.py
+++ neurobooth_os/tasks/fixations.py
@@ -12,6 +12,9 @@ Created on Mon Nov 22 11:22:32 2021
 @author: STM
 """
 
+import os.path as op
+
+import neurobooth_os
 from neurobooth_os.tasks.task import Task_Eyetracker
 
 
@@ -26,6 +29,17 @@ class Fixation_Target(Task_Eyetracker):
         self.present_text(screen=self.target, msg='task', audio=None, wait_time=duration, waitKeys=False)
         
         if prompt:
+            func_kwargs = locals()
+            del func_kwargs['self']
+            print(func_kwargs)
             self.present_text(screen=self.press_task_screen, msg='task-continue-repeat', func=self.present_task,
-                          func_kwargs=locals(), waitKeys=False)
-  
+                          func_kwargs=func_kwargs, waitKeys=False)
+
+
+if __name__ == "__main__":
+
+    # task = Task(instruction_file=op.join(neurobooth_os.__path__[0], 'tasks', 'assets', 'test.mp4'))
+    # task.run() 
+
+    task = Fixation_Target(instruction_file=op.join(neurobooth_os.__path__[0], 'tasks', 'assets', 'test.mp4'))
+    task.run(prompt=True, duration=3,  target_pos=(-10,-5))
