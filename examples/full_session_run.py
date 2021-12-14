96fd46cba31132e3b20940466b2cbddceb496fe0
adonunes <adonay.s.nunes@gmail.com>
2021-12-14 16:42:41 -0500

    Edits example run session

--- examples/full_session_run.py
+++ examples/full_session_run.py
@@ -12,25 +12,25 @@ from neurobooth_os.iout import marker_stream
 from neurobooth_os.gui import _process_received_data, ctr_event_handler
 from neurobooth_os.layouts import _main_layout, _win_gen
 import neurobooth_os.iout.metadator as meta
-from neurobooth_os.tasks.task_importer import get_task_funcs
 
-def event_listener(window, nodes, timeout=1):
+
+def event_listener(window, timeout=1):
     while True:
         event, values = window.read(timeout)
         yield event, values
 
 # Define parameters
-remote = True
-database_name = "mock_neurobooth"
-collection_id = "mock_collection"  # Define mock with tasks to run
-sess_info = {"subj_id":"test_nogui",
+remote = False
+database_name = "neurobooth"  # "mock_neurobooth"
+collection_id = "mvp_030"  # "mock_collection"  # Define mock with tasks to run
+sess_info = {"subj_id": "69",  #"test_nogui",
              "staff_id":"AN", 
              '_tasks_': "mock_task_1"}
 
 nodes = ('dummy_acq', 'dummy_stm')
 
 tech_obs_log = meta._new_tech_log_dict()
-tech_obs_log["study_id"] = 'mock_study'
+tech_obs_log["study_id"] = 'study1'
 tech_obs_log["collection_id"] = collection_id
 tech_obs_log["staff_id"] = sess_info['staff_id']
 tech_obs_log["subject_id"] = sess_info['subj_id']
@@ -49,7 +49,7 @@ else:
 conn = meta.get_conn(remote=remote, database=database_name)
 
 # Create mock CTR
-window = _win_gen(_main_layout, sess_info, remote)
+window = _win_gen(_main_layout, sess_info, True)
 event, values = window.read(.1)
 ctr_host, ctr_port = node_info(ctr_node)   
 callback, callback_args = _process_received_data, window    
@@ -87,30 +87,41 @@ ctr_rec.prepare_devices(f"{collection_id}:{str(tech_obs_log)}", nodes=nodes)
 out["vidf_mrkr"] = vidf_mrkr
 
 out['exit_flag'] ='prepared'
-for event, values in event_listener(window, nodes, timeout=1):
+for event, values in event_listener(window, timeout=1):
     out = ctr_event_handler(window, event, values, conn, study_id_date, statecolors, stream_ids, inlets, out)
     if out["break_"]:
         print("exiting prepare loop")
         out["break_"] = False
         break
         
-# Run tasks
-tasks = list(get_task_funcs(collection_id, conn))
+# Get tasks
+tasks_obs = meta.get_tasks(collection_id, conn)
+tasks = []
+for task in tasks_obs:
+    task_id, *_ = meta._get_task_param(task, conn)
+    tasks.append(task_id)
 print(tasks)
+
+tasks = ['intro_occulo_task_1', 'pursuit_task_1', 'intro_cog_task_1', 'sit_to_stand_task_1']
 
 if len(tasks):
     running_task = "-".join(tasks)  # task_name can be list of task1-task2-task3
     ctr_rec.task_presentation(running_task, subj_id, node=nodes[1])
 else:
-    sg.PopupError('No task selected')
+    print('No task selected')
 
-for task in tasks:
-    for event, values in  event_listener(window, nodes, timeout=1):
-        out['exit_flag'] ='task_end'
+# Present tasks
+out['exit_flag'] ='task_end'
+# remove calibration and intro tasks as wont signal task finished
+tasks_no_introcal = [ t for t in tasks if  not any(x for x in ['calibration_task', "intro_"] if x in t)]
+for task in tasks_no_introcal:
+    for event, values in  event_listener(window, timeout=1):
         out = ctr_event_handler(window, event, values, conn, study_id_date, statecolors, stream_ids, inlets, out)
         if out["break_"] == True:
             out["break_"] = False
+            print(f'out task {task}')
             break
 
 # Close
 ctr_rec.shut_all(nodes=nodes)
+window.close()
