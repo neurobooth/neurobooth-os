7b140c2bd5ce89fc91385096201a22cfa08475cf
adonunes <adonay.s.nunes@gmail.com>
2021-12-15 12:56:57 -0500

    Flir realtime rec

--- neurobooth_os/iout/flir_cam.py
+++ neurobooth_os/iout/flir_cam.py
@@ -24,14 +24,13 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 
 class VidRec_Flir():
-    def __init__(self, fourcc=cv2.VideoWriter_fourcc(*'MJPG'),
+    def __init__(self, 
                  sizex=round(1936 / 2), sizey=round(1216 / 2), fps=196,
                  camSN="20522874", exposure=4500, gain=20, gamma=.6,
-                 device_id="FLIR_blackfly_1", sensor_ids=['FLIR_rgb_1']):
+                 device_id="FLIR_blackfly_1", sensor_ids=['FLIR_rgb_1'], fd= .6):
 
         self.open = False
         self.serial_num = camSN
-        self.fourcc = fourcc
         self.fps = fps
         self.serial_num = camSN
         self.exposure = exposure
@@ -39,8 +38,8 @@ class VidRec_Flir():
         self.gamma = gamma
         self.device_id = device_id
         self.sensor_ids = sensor_ids
+        self.fd = fd
         self.recording = False
-        
         self.get_cam()
         self.setup_cam()
 
@@ -72,12 +71,8 @@ class VidRec_Flir():
     def createOutlet(self):
         self.streamName = 'FlirFrameIndex'
         self.oulet_id = str(uuid.uuid4())
-        info = StreamInfo(
-            name=self.streamName,
-            type='videostream',
-            channel_format='int32',
-            channel_count=2,
-            source_id=self.oulet_id)
+        info = StreamInfo(name=self.streamName, type='videostream', channel_format='int32',
+            channel_count=2, source_id=self.oulet_id)
 
         info.desc().append_child_value("device_id", self.device_id)
         info.desc().append_child_value("sensor_ids", str(self.sensor_ids))
@@ -96,89 +91,52 @@ class VidRec_Flir():
     # from buffer in separate process
     def camCaptureVid(self):
         while self.recording or self.image_queue.qsize():
-            # get images formated as numpy from separate process queue
             dequeuedImage = self.image_queue.get(block=True, timeout=1)
-
-            # self.writer.writeFrame(dequeuedImage)
-            # self.writer.write(dequeuedImage)
-            # self.image_queue.task_done()
-
+            self.video_out.write(dequeuedImage)
+            
     def start(self, name="temp_video"):
         self.prepare(name)
         self.video_thread = threading.Thread(target=self.record)
         self.video_thread.start()
 
-    def prepare(self, name="temp_video"):
-        self.cam.BeginAcquisition()
+    def imgage_proc(self):
         im = self.cam.GetNextImage(1000)
-        self.frameSize = (im.GetWidth(), im.GetHeight())
+        tsmp = im.GetTimeStamp()
+        imgarr = im.GetNDArray()
+        im_conv = cv2.demosaicing(imgarr, cv2.COLOR_BayerBG2BGR)
+        im.Release()
+        return  cv2.resize(im_conv, None, fx=self.fd, fy=self.fd), tsmp 
+        
+    def prepare(self, name="temp_video"):
+        self.cam.BeginAcquisition()  
+        im, _ = self.imgage_proc()
+        self.frameSize = (im.shape[1], im.shape[0])
         self.video_filename = "{}_flir.avi".format(name)
-        # self.video_out = cv2.VideoWriter(self.video_filename, self.fourcc,
-        #                                  self.fps, self.frameSize)
+        
+        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+        self.FRAME_RATE_OUT = self.cam.AcquisitionResultingFrameRate()
+        self.video_out = cv2.VideoWriter(self.video_filename, fourcc,
+                                          self.FRAME_RATE_OUT, self.frameSize)
         print(f"-new_filename-:{self.streamName}:{op.split(self.video_filename)[-1]}")
 
         self.streaming = True
 
-        # setup output video file parameters (can try H265 in future for better compression):
-        # for some reason FFMPEG takes exponentially longer to write at
-        # nonstandard frame rates, so just use default 25fps and change elsewhere
-        # if needed
-        self.FRAME_RATE_OUT = self.cam.AcquisitionResultingFrameRate()
-        print('frame rate = {:.2f} FPS'.format(self.FRAME_RATE_OUT))
-        self.crfOut = 21  # controls tradeoff between quality and storage, see https://trac.ffmpeg.org/wiki/Encode/H.264
-        ffmpegThreads = 10  # this controls tradeoff between CPU usage and memory usage; video writes can take a long time if this value is low
-        # crfOut = 18 #this should look nearly lossless
-        # writer = skvideo.io.FFmpegWriter(movieName, outputdict={'-r': str(FRAME_RATE_OUT), '-vcodec': 'libx264', '-crf': str(crfOut)}) # with frame rate
-        # self.writer = skvideo.io.FFmpegWriter(self.video_filename,
-        #                                        inputdict={'-r':  str(self.FRAME_RATE_OUT), '-s':'{}x{}'.format(self.frameSize[0],self.frameSize[1])},
-        # outputdict={'-r': str(self.FRAME_RATE_OUT), '-c:v': 'libx264', '-crf':
-        # str(self.crfOut), '-preset': 'ultrafast'})#, , '-pix_fmt': 'yuv444p'})#,
-
-        fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-        # self.writer = cv2.VideoWriter(self.video_filename, fourcc, self.FRAME_RATE_OUT, self.frameSize)# 115, self.frameSize)#
-        # fps = self.FRAME_RATE_OUT
-
-        # crf = 17
-        # self.writer = skvideo.io.FFmpegWriter(self.video_filename,
-        #             inputdict={ '-s':'{}x{}'.format(self.frameSize[0], self.frameSize[1])},
-        # outputdict={'-r': str(fps), '-c:v': 'ffv1', '-crf': str(crf), '-preset':
-        # 'ultrafast', '-pix_fmt': 'yuv444p'})
-
     def record(self):
         self.recording = True
         self.frame_counter = 0
-        # self.save_thread = threading.Thread(target=self.camCaptureVid)
-        # self.save_thread.start()
+        self.save_thread = threading.Thread(target=self.camCaptureVid)
+        self.save_thread.start()
         print(f"FLIR recording {self.video_filename}")
         t0 = time.time()
         self.stamp = []
         while self.recording:
-            im = self.cam.GetNextImage(1000)
-            tsmp = im.GetTimeStamp()
-            imgarr = im.GetNDArray()
-            im_conv = cv2.demosaicing(imgarr, cv2.COLOR_BayerBG2BGR)
-            im_conv = cv2.cvtColor(im_conv, cv2.COLOR_BGR2RGB)
-            # self.image_queue.put(im_conv)
-
-            # im_conv = im.Convert(PySpin.PixelFormat_BGR8, PySpin.HQ_LINEAR)
-            # im_conv_d = im_conv.GetData()
-            # im_conv_d = im_conv_d.reshape((im.GetHeight(), im.GetWidth(), 3) )
-            # self.image_queue.put(im_conv_d)
-
-            #   stamp.append(time.time_ns())
+            im, tsmp = self.imgage_proc()
+            self.image_queue.put(im)
             self.stamp.append(tsmp)
-
-            #  Release image
-            #
-            #  *** NOTES ***
-            #  Images retrieved directly from the camera (i.e. non-converted
-            #  images) need to be released in order to keep from filling the
-            #  buffer.
-            im.Release()
 
             try:
                 self.outlet.push_sample([self.frame_counter, tsmp])
-            except BaseException:  # "OSError" from C++
+            except BaseException:
                 print(f"Reopening FLIR {self.device_index} stream already closed")
                 self.outlet = self.createOutlet(self.video_filename)
                 self.outlet.push_sample([self.frame_counter])
@@ -192,9 +150,9 @@ class VidRec_Flir():
         print(f"FLIR recording ended with {self.frame_counter} frames in {time.time()-t0}")
         self.cam.EndAcquisition()
         self.recording = False
-        # self.save_thread.join()
+        self.save_thread.join()
         # self.writer.close()
-        # self.writer.release()
+        self.video_out.release()
         print(f"FLIR video saving ended in {time.time()-t0} sec")
 
     def stop(self):
@@ -212,11 +170,11 @@ class VidRec_Flir():
 
 if __name__ == "__main__":
 
-    ximi = VidRec_Flir()
-    ximi.start()
+    flir = VidRec_Flir()
+    flir.start()
     time.sleep(10)
-    ximi.close()
-    tdiff = np.diff(ximi.stamp) / 1e6
+    flir.close()
+    tdiff = np.diff(flir.stamp) / 1e6
     plt.figure(), plt.hist(tdiff, 50)
     plt.figure(), plt.plot(tdiff)
     print("diff max min", tdiff.max() - tdiff.min())
