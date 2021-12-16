1f443e3f14a55a92ccc3f677892a213196e85bd4
Mainak Jas <mainakjas@gmail.com>
2021-12-16 18:17:36 -0500

    FIX mock database

--- neurobooth_os/mock/mock_database.py
+++ neurobooth_os/mock/mock_database.py
@@ -20,30 +20,31 @@ def insert_mock_rows(conn_mock):
     table_ids = list_tables(conn_mock)
 
     table = Table('study', conn_mock)
-    table.insert_rows([('mock_study', 0, 'mock_study', 0, 0,
-                        '{mock_collection}', None, None)])
+    table.insert_rows([('mock_study', 'mock_study', '{mock_collection}', 0)],
+                        cols=['study_id', 'study_title', 'collection_ids', 'IRB_protocol_number'])
 
     table = Table('collection', conn_mock)
-    table.insert_rows([('mock_collection', False, '{mock_obs_1}', None)])
+    table.insert_rows([('mock_collection', '{mock_obs_1}')],
+                      cols=['collection_id', 'tech_obs_array'])
 
     table = Table('stimulus', conn_mock)
     table.insert_rows([('mock_task_1', 'description', 2, 5,                        
-                        'mock.mock_task.py::MockTask()')],
+                        'mock.mock_task.py::MockTask()', 'mp4')],
                         cols=['stimulus_id', 'stimulus_description', 'num_iterations', "duration",
-                         'stimulus_file'])
+                         'stimulus_file', 'stimulus_filetype'])
 
-    instruction_id = Table('instruction', conn_mock).insert_rows(
-        [("mock task, follow the instructions",)], cols=['instruction_text'])
+    Table('instruction', conn_mock).insert_rows(
+        [('mock_instruction_id', "mock task, follow the instructions",)], cols=['instruction_id', 'instruction_text'])
 
     table = Table('tech_obs_data', conn_mock)
-    table.insert_rows([('mock_obs_1',  instruction_id, 'mock_task_1',
+    table.insert_rows([('mock_obs_1', 'mock_instruction_id', 'mock_task_1',
                         '{mock_Mbient_1,mock_Mbient_2, mock_Intel_1}',                        
                         ('{{mock_Mbient_acc_1, mock_Mbient_grad_1},'
                          '{mock_Mbient_acc_1, mock_Mbient_grad_1},'
                          '{mock_Intel_rgb_1, mock_Intel_depth_1}}')
-                         )], 
+                         , "hand")], 
                          cols=["tech_obs_id", "instruction_id", "stimulus_id", "device_id_array",
-                          "sensor_id_array" ])
+                          "sensor_id_array", "feature_of_interest" ])
 
     table = Table('device', conn_mock)
     table.insert_rows([('mock_dev_1', 0, False, 0, 'mock', 'mock_make',
