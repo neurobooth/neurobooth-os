2f882e8b8c5617dbc28c29f6d60ad6e221eac2c6
Mainak Jas <mainakjas@gmail.com>
2021-12-17 15:58:33 -0500

    Simplify full_session_run

--- examples/full_session_run.py
+++ examples/full_session_run.py
@@ -1,138 +1,74 @@
-
-import threading
-import sys 
-import PySimpleGUI as sg
-from time import sleep as tsleep
-from datetime import datetime
-
-import neurobooth_os.main_control_rec as ctr_rec
-from neurobooth_os.netcomm import get_messages_to_ctr, node_info, NewStdout
+from neurobooth_os.netcomm import node_info
 import neurobooth_os.iout.metadator as meta
-from neurobooth_os.iout import marker_stream
-from neurobooth_os.gui import _process_received_data, ctr_event_handler
-from neurobooth_os.layouts import _main_layout, _win_gen
-import neurobooth_os.iout.metadator as meta
-
-
-def event_listener(window, timeout=1):
-    while True:
-        event, values = window.read(timeout)
-        yield event, values
+from neurobooth_os.gui import (_find_subject, _select_subject, _get_tasks,
+                               _save_session, _get_collections,
+                               _start_ctr_server, _start_servers,
+                               _prepare_devices, _start_task_presentation)
 
 # Manually define parameters
 remote = True
-database_name = "neurobooth"  # "mock_neurobooth"
-study_id = 'study1'
-collection_id = "mvp_030"  # "mock_collection"  # Define mock with tasks to run
-subject_id = "69"
-staff_id = "AN"
-
-# Variables setup
-sess_info = {"subject_id": subject_id,  #"test_nogui",
-             "staff_id": staff_id, 
-             '_tasks_': "task_1",
-             'first_name': 'Name',
-             'last_name': 'Surname'}
-
-nodes = ('dummy_acq', 'dummy_stm')
-
-tech_obs_log = meta._new_tech_log_dict()
-tech_obs_log["study_id"] = study_id
-tech_obs_log["collection_id"] = collection_id
-tech_obs_log["staff_id"] = sess_info['staff_id']
-tech_obs_log["subject_id"] = sess_info['subj_id']
-study_id_date = f'{sess_info["subj_id"]}_{datetime.now().strftime("%Y-%m-%d")}'
-tech_obs_log["study_id-date"] = study_id_date
-subj_id = sess_info['subj_id']
 
 # Get DB connexion
 if remote:
-    ctr_node = "dummy_ctr"
+    database = "mock_neurobooth"
     nodes = ('dummy_acq', 'dummy_stm')
+    host_ctr, port_ctr = node_info("dummy_ctr")
 else:
-    ctr_node = "control"
-    nodes = ("acquisition", "presentation")
-    
-conn = meta.get_conn(remote=remote, database=database_name)
+    nodes = ('acquisition', 'presentation')
+    host_ctr, port_ctr = node_info("control")
 
-# Create mock CTR
-window = _win_gen(_main_layout, sess_info, True)
-event, values = window.read(.1)
-ctr_host, ctr_port = node_info(ctr_node)   
-callback, callback_args = _process_received_data, window    
-server_thread = threading.Thread(target=get_messages_to_ctr,
-                                    args=(callback, remote, ctr_host, ctr_port, callback_args,),
-                                    daemon=True)
-server_thread.start()
-tsleep(.3)
+# collection_id = "mvp_030"  # "mock_collection"  # Define mock with tasks to run
 
-if remote:
-    # Rerout stdout to dummy_ctr and print in terminal
-    sys.stdout = NewStdout("mock",  target_node=ctr_node, terminal_print=True)
+steps = list()
+tech_obs_log = meta._new_tech_log_dict()
 
-# Create servers
-ctr_rec.start_servers(nodes=nodes, remote=remote, conn=conn)
-tsleep(.3)
+class FakeGUIElement(dict):
+    def get_indexes(self):
+        return 0
+    def Update(self, button_color):
+        pass
 
-# automatically run session
-stream_ids, inlets,  = {}, {}
-out = dict(exit_flag=None,  # where to break
-    break_ = False,  # break loop if True                
-    obs_log_id = None, 
-    t_obs_id = None, 
-    task_id = None,
-    )
-        
-statecolors = {"-init_servs-": ["green", "yellow"],
-                "-Connect-": ["green", "yellow"],
-               }
+class FakeWindow(dict):
+    def read(self, arg1):
+        return (0, 0)
+    def write_event_value(self, arg1, arg2):
+        return
+    def close(self):
+        pass
 
-# Prepare devices and streams
-vidf_mrkr = marker_stream('videofiles')
-window.write_event_value('-OUTLETID-', f"['{vidf_mrkr.name}', '{vidf_mrkr.outlet_id}']")
-ctr_rec.prepare_devices(f"{collection_id}:{str(tech_obs_log)}", nodes=nodes)
-out["vidf_mrkr"] = vidf_mrkr
+fake_gui_element = FakeGUIElement()
+fake_window1 = FakeWindow(
+    {'dob': fake_gui_element, 'first_name': fake_gui_element,
+     'last_name': fake_gui_element, 'collection_id': fake_gui_element,
+     'tasks': fake_gui_element})
 
-out['exit_flag'] ='prepared'
-for event, values in event_listener(window, timeout=1):
-    out = ctr_event_handler(window, event, values, conn, study_id_date, statecolors, stream_ids, inlets, out)
-    if out["break_"]:
-        print("exiting prepare loop")
-        out["break_"] = False
-        break
-        
-# Get tasks
-tasks_obs = meta.get_tasks(collection_id, conn)
-tasks = []
-for task in tasks_obs:
-    task_id, *_ = meta._get_task_param(task, conn)
-    tasks.append(task_id)
-print(tasks)
-# tasks = ['intro_occulo_task_1', 'pursuit_task_1', 'intro_cog_task_1', 'sit_to_stand_task_1']
+fake_window2 = FakeWindow(
+    {'-init_servs-': fake_gui_element, '-Connect-': fake_gui_element,
+     'Start': fake_gui_element}
+)
 
-if len(tasks):
-    running_task = "-".join(tasks)  # task_name can be list of task1-task2-task3
-    ctr_rec.task_presentation(running_task, subj_id, node=nodes[1])
-else:
-    print('No task selected')
+conn = meta.get_conn(remote=remote, database=database)
 
-# Present tasks
-out['exit_flag'] ='task_end'
-# remove calibration and intro tasks as wont signal task finished
-tasks_no_introcal = [ t for t in tasks if  not any(x for x in ['calibration_task', "intro_"] if x in t)]
-for task in tasks_no_introcal:
-    for event, values in  event_listener(window, timeout=1):
-        _record_lsl(window, session, subject_id, task_id,
-                    t_obs_id, obs_log_id, tsk_strt_time)
-        _stop_lsl_and_save(window, session, conn,
-                           rec_fname, task_id, tech_obs_log_id,
-                           t_obs_id)
+first_name, last_name = "Anna", "Luddy"
+subject_df = _find_subject(fake_window1, conn, first_name, last_name)
+first_name, last_name, subject_id = _select_subject(fake_window1, subject_df)
 
-        if out["break_"] == True:
-            out["break_"] = False
-            print(f'out task {task}')
-            break
+study_id = 'mock_study'
+tech_obs_log["study_id"] = study_id
+collection_ids = _get_collections(fake_window1, conn, study_id)
 
-# Close
-ctr_rec.shut_all(nodes=nodes)
+collection_id = collection_ids[0]
+tasks = _get_tasks(fake_window1, conn, collection_id)
+
+staff_id = 'AN'
+sess_info = _save_session(fake_window1,
+                          tech_obs_log, staff_id,
+                          subject_id, first_name, last_name, tasks)
+window = _start_ctr_server(host_ctr, port_ctr, sess_info, remote=remote)
 window.close()
+
+_start_servers(fake_window2, conn, nodes, remote=True)
+vidf_mrkr, _, _ = _prepare_devices(fake_window2, nodes, collection_id,
+                                   tech_obs_log)
+_start_task_presentation(fake_window2, tasks, sess_info['subject_id'], steps,
+                         node=nodes[1])
