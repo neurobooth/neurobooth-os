590ea35e22b54df6474a672665a3d1f1cf84607a
Mainak Jas <mainakjas@gmail.com>
2021-12-23 15:18:53 -0500

    Use queue not dict

--- neurobooth_os/mock/gui.py
+++ neurobooth_os/mock/gui.py
@@ -20,17 +20,16 @@ class MockWindow(dict):
     def __init__(self, gui_element_names):
         mapping = {name: MockGUIElement() for name in gui_element_names}
         super(MockWindow, self).__init__(mapping)
-        self.events = dict()
+        self.events = list()
 
     def read(self, timeout):
         time.sleep(timeout)
-        try:
-            return self.events.popitem()
-        except:
-            return (None, None)
+        if len(self.events) > 0:
+            return self.events.pop(0)
+        return (None, None)
 
     def write_event_value(self, key, val):
-        self.events[key] = val
+        self.events.append((key, val))
 
     def close(self):
         pass
