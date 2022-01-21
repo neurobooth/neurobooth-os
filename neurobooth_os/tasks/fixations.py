da1f44fc4c06eeaf679fecea8d65b3e27e757ff5
adonunes <adonay.s.nunes@gmail.com>
2022-01-21 14:44:04 -0500

    remove fixation targ deg, exemple run close win

--- neurobooth_os/tasks/fixations.py
+++ neurobooth_os/tasks/fixations.py
@@ -41,8 +41,6 @@ class Fixation_Target_Multiple(Task_Eyetracker):
         super().__init__(**kwargs)
     
     def present_task(self, prompt=True, duration=3, trial_pos=[(0,0), (0,15)], target_size=25, **kwargs):
-
-        
         self.countdown_task()
         for pos in trial_pos:
             self.target.pos = [self.deg_2_pix(pos[0])/2, self.deg_2_pix(pos[1])/2] 
@@ -61,7 +59,6 @@ class Fixation_Target_sidetrials(Task_Eyetracker):
         super().__init__(**kwargs)
     
     def present_task(self, prompt=True, duration=3, target_pos=(-10,10), target_size=25, trial_intruct=['trial 1', 'trial 2'], **kwargs):
-        self.target.units = 'deg'
         
         for nth, trl in enumerate(trial_intruct):            
             msg = utils.create_text_screen(self.win, trl  + "\n\n\nPress CONTINUE")
@@ -96,5 +93,5 @@ if __name__ == "__main__":
     t.run(duration=3, trial_pos=[(0,15), (30,15), (-30, 15)], target_size=25)
     
     t= Fixation_Target_sidetrials()
-    t.run(duration=3, target_pos=(0,20), target_size=25, trial_intruct=["Do the task with your DOMINANT arm", "Do the task with your NON-DOMINANT arm"])         
+    t.run(duration=3, target_pos=(0,15), target_size=80, trial_intruct=["Do the task with your DOMINANT arm", "Do the task with your NON-DOMINANT arm"])         
 
