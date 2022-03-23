63c65c9d06315b8c918ff540fab178a19bab4a7e
adonunes <adonay.s.nunes@gmail.com>
2022-03-23 14:01:38 -0400

    Add session path save notes

--- neurobooth_os/layouts.py
+++ neurobooth_os/layouts.py
@@ -219,7 +219,7 @@ def write_task_notes(subject_id, staff_id, task_name, task_notes):
         The task notes.
     """
 
-    fname = f'{cfg.paths["data_out"]}{subject_id}-{task_name}-notes.txt'
+    fname = f'{cfg.paths["data_out"]}{subject_id}/{subject_id}-{task_name}-notes.txt'
     task_txt = ''
     if not op.exists(fname):
         task_txt += f'{subject_id}, {staff_id}\n'
