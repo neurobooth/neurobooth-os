ccab6e1eb589e612e3eb74d841696a1cc0e80e87
Mainak Jas <mainakjas@gmail.com>
2022-04-01 18:51:59 -0400

    tech_obs -> task

--- neurobooth_os/iout/tests/test_metadator.py
+++ neurobooth_os/iout/tests/test_metadator.py
@@ -4,22 +4,22 @@ from neurobooth_os.iout import metadator as meta
 
 
 
-def test_tech_obs_addition():
+def test_task_addition():
 
     conn = meta.get_conn(remote=True,database="mock_neurobooth")
     subj_id = "Test"
-    tech_obs_id = meta._make_new_tech_obs_row(conn, subj_id)
+    task_id = meta._make_new_task_row(conn, subj_id)
 
     vals_dict = meta._new_tech_log_dict()
     vals_dict['subject_id'] = subj_id
     vals_dict['study_id'] = "mock_study"
-    vals_dict['tech_obs_id'] = "mock_obs_1"
+    vals_dict['task_id'] = "mock_obs_1"
     vals_dict['staff_id'] = "mocker"    
     vals_dict['event_array'] = "event:datestamp"
     vals_dict['collection_id'] = "mock_collection"
     vals_dict['site_id'] = "mock_site"
 
-    meta._fill_tech_obs_row(tech_obs_id, vals_dict, conn)
+    meta._fill_task_row(task_id, vals_dict, conn)
     
 
 
