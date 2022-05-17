f1ec70c2b6e3da33b7fb323e944655afe2323d31
adonunes <adonay.s.nunes@gmail.com>
2022-05-17 16:53:48 -0400

    auto expo low priority

--- neurobooth_os/iout/mbient.py
+++ neurobooth_os/iout/mbient.py
@@ -159,7 +159,7 @@ class Sensor:
         libmetawear.mbl_mw_debug_reset(self.device.board)
         print("Stopped ", self.dev_name)
         self.streaming = False
-        e.wait()
+        e.wait(10)
         
         
     def close(self):
