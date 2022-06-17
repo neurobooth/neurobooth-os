6d256ead30c1ae857c89ad95a03f5da15628b2ce
adonunes <adonay.s.nunes@gmail.com>
2022-06-17 18:16:04 -0400

    FIX mbient time warp, mic, new iphone app

--- extras/mbient_hdf5_inspect.py
+++ extras/mbient_hdf5_inspect.py
@@ -8,9 +8,10 @@ Created on Tue May  3 15:35:18 2022
 import glob
 from h5io import read_hdf5
 import os.path as op
+import numpy as np
 
 session = '100089_2022-06-01'
-
+session = '100064_2022-06-03'
 files = glob.glob(f"Z:/data/{session}/{session}*Mbient*.hdf5")
 
 for fl in files:
@@ -19,4 +20,4 @@ for fl in files:
     fps = data['device_data']['time_stamps']
     
     _, name = op.split(fl)
-    print(name.replace(session, '').replace("-mbient_acc_1-mbient_gyro_1.hdf5", ""), len(fps))
+    print(name.replace(session, '').replace("-mbient_acc_1-mbient_gyro_1.hdf5", ""), len(fps), 1/np.mean(np.diff(fps)))
