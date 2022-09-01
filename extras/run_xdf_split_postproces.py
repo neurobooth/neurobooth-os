3e9939ff7cfda017d4f114c02eb548292b5237b3
adonunes <adonay.s.nunes@gmail.com>
2022-09-01 15:14:32 -0400

    add extras and timing task

---
+++ extras/run_xdf_split_postproces.py
@@ -0,0 +1,8 @@
+
+from neurobooth_os.iout.metadator import get_conn
+from neurobooth_os.iout.split_xdf import create_h5_from_csv
+
+conn = get_conn()
+#location of the csv file containing path filename and task ID
+dont_split_xdf_fpath = "C:/neurobooth"
+create_h5_from_csv(dont_split_xdf_fpath, conn)
