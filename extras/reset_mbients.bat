565de0f7f53dfca60e3c7a5a5152dd2395c75466
adonunes <adonay.s.nunes@gmail.com>
2022-10-14 16:38:21 -0400

    adding reset mbients py and bat modified for Wang mbients serial numbers

--- extras/reset_mbients.bat
+++ extras/reset_mbients.bat
@@ -1,2 +1,2 @@
-call C:\Users\CTR\anaconda3\Scripts\activate.bat C:\Users\CTR\anaconda3\envs\neurobooth
+call C:\Users\ACQ\anaconda3\Scripts\activate.bat C:\Users\ACQ\anaconda3\envs\neurobooth-staging
 call start /W ipython C:\neurobooth-os\extras\reset_mbients.py
