331479041fa45c63db2d77684885ef0137b49f14
adonunes <adonay.s.nunes@gmail.com>
2022-10-14 16:48:00 -0400

    adding updated post split xdf bat

--- extras/postprocess_split_xdf.bat
+++ extras/postprocess_split_xdf.bat
@@ -1,2 +1,2 @@
-call  C:\Users\CTR\anaconda3\Scripts\activate.bat C:\Users\CTR\anaconda3\envs\neurobooth
-call start /W ipython C:\neurobooth-eel\extras\run_xdf_split_postproces.py
+call  C:\Users\CTR\anaconda3\Scripts\activate.bat C:\Users\CTR\anaconda3\envs\neurobooth-staging
+call start /W ipython C:\neurobooth-os\extras\run_xdf_split_postproces.py
