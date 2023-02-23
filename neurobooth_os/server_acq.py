f6f1451f54f8f25365496151525626292aa61594
Brandon Oubre <boubre@mgh.harvard.edu>
2023-02-23 17:38:13 -0500

    Refactoring server_acq to 1) make it easier to see a sky-high view of the main server loop, 2) make global server state more obvious, and 3) group together common functionality/state.

--- neurobooth_os/server_acq.py
+++ neurobooth_os/server_acq.py
@@ -1,11 +1,8 @@
 import socket
 import os
 import sys
-from time import time, sleep
-from collections import OrderedDict
-import cv2
-import numpy as np
-from pylsl import local_clock
+from typing import Dict, List, Any
+from time import time
 
 import neurobooth_os
 from neurobooth_os import config
@@ -15,17 +12,8 @@ from neurobooth_os.iout.lsl_streamer import (
     start_lsl_threads,
     close_streams,
     reconnect_streams,
-    connect_mbient,
 )
 import neurobooth_os.iout.metadator as meta
-
-
-def countdown(period):
-    t1 = local_clock()
-    t2 = t1
-
-    while t2 - t1 < period:
-        t2 = local_clock()
 
 
 def Main():
@@ -34,117 +22,178 @@ def Main():
     sys.stdout = NewStdout("ACQ", target_node="control", terminal_print=True)
     s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    streams = {}
-    lowFeed_running = False
+    # The following variables are global server state shared across successive messages
+    device_streams = DeviceStreams()
+    low_feed = LowFeed()
     recording = False
+    subject_id_date = None
+    task = None
+
+    # Infinite loop - process incoming messages
     for data, connx in get_client_messages(s1):
+        # print(f'Message recieved: {data}')
 
         if "vis_stream" in data:
-            if not lowFeed_running:
-                lowFeed = VidRec_Brio(
-                    camindex=config.paths["cam_inx_lowfeed"], doPreview=True
-                )
-                print("LowFeed running")
-                lowFeed_running = True
-            else:
-                print(f"-OUTLETID-:Webcam:{lowFeed.preview_outlet_id}")
-                print("Already running low feed video streaming")
-
+            low_feed.viz_stream()
         elif "prepare" in data:
-            # data = "prepare:collection_id:database:str(log_task_dict)"
-            collection_id = data.split(":")[1]
-            database_name = data.split(":")[2]
-            log_task = eval(
-                data.replace(f"prepare:{collection_id}:{database_name}:", "")
-            )
-            subject_id_date = log_task["subject_id-date"]
-
-            conn = meta.get_conn(database=database_name)
-            ses_folder = f"{config.paths['data_out']}{subject_id_date}"
-            if not os.path.exists(ses_folder):
-                os.mkdir(ses_folder)
-
-            task_devs_kw = meta._get_device_kwargs_by_task(collection_id, conn)
-            if len(streams):
-                # print("Checking prepared devices")
-                streams = reconnect_streams(streams)
-            else:
-                streams = start_lsl_threads("acquisition", collection_id, conn=conn)
-
-            devs = list(streams.keys())
-            print("UPDATOR:-Connect-")
-
+            subject_id_date = prepare(device_streams, data)
         elif "frame_preview" in data and not recording:
-            if not any("IPhone" in s for s in streams):
-                print("no iphone")
-                connx.send("ERROR: no iphone in LSL streams".encode("ascii"))
-                continue
-
-            frame = streams[[i for i in streams if "IPhone" in i][0]].frame_preview()
-            frame_prefix = b"::BYTES::" + str(len(frame)).encode("utf-8") + b"::"
-            frame = frame_prefix + frame
-            connx.send(frame)
-
+            frame_preview(device_streams, connx)
         elif "record_start" in data:
-            # "record_start::filename::task_id" FILENAME = {subj_id}_{obs_id}
-            print("Starting recording")
-            t0 = time()
-            fname, task = data.split("::")[1:]
-            fname = f"{config.paths['data_out']}{subject_id_date}/{fname}"
-
-            for k in streams.keys():
-                if k.split("_")[0] in ["hiFeed", "FLIR", "Intel", "IPhone"]:
-                    if task_devs_kw[task].get(k):
-                        try:
-                            streams[k].start(fname)
-                        except:
-                            continue
-
-            for k in streams.keys():
-                if "Mbient" in k:
-                    try:
-                        if not streams[k].device.is_connected:
-                            streams[k].try_reconnect()
-                    except Exception as e:
-                        print(e)
-                        pass
-            print(f"Device start took {time() - t0:.2f}")
-            msg = "ACQ_devices_ready"
-            connx.send(msg.encode("ascii"))
+            task = record_start(device_streams, subject_id_date, data, connx)
             recording = True
-
         elif "record_stop" in data:
-            t0 = time()
-            for k in streams.keys():
-                if k.split("_")[0] in ["hiFeed", "FLIR", "Intel", "IPhone"]:
-                    if task_devs_kw[task].get(k):
-                        streams[k].stop()
-
-            print(f"Device stop took {time() - t0:.2f}")
-            msg = "ACQ_devices_stoped"
-            connx.send(msg.encode("ascii"))
+            record_stop(device_streams, task, connx)
             recording = False
-
-        elif data in ["close", "shutdown"]:
-            if "shutdown" in data:
-                sys.stdout = sys.stdout.terminal
-                s1.close()
-
-            streams = close_streams(streams)
-
-            if "shutdown" in data:
-                if lowFeed_running:
-                    lowFeed.close()
-                    lowFeed_running = False
-                    print("Closing RTD cam")
-                break
-
+        elif "close" in data:
+            device_streams.close()
+        elif "shutdown" in data:
+            sys.stdout = sys.stdout.terminal
+            s1.close()
+            device_streams.close()
+            low_feed.close()
+            break  # Exit infinite message loop
         elif "time_test" in data:
-            msg = f"ping_{time()}"
-            connx.send(msg.encode("ascii"))
-
+            connx.send(f"ping_{time()}".encode("ascii"))
         else:
-            print(data)
+            print(f'Unexpected message: {data}')
+
+
+class LowFeed:
+    stream: VidRec_Brio
+    running: bool
+
+    def __init__(self):
+        self.running = False
+
+    def viz_stream(self) -> None:
+        if not self.running:
+            self.stream = VidRec_Brio(
+                camindex=config.paths['cam_inx_lowfeed'],
+                doPreview=True,
+            )
+            self.running = True
+            print("LowFeed running")
+        else:
+            print(f"-OUTLETID-:Webcam:{self.stream.preview_outlet_id}")
+            print("Already running low feed video streaming")
+
+    def close(self) -> None:
+        if self.running:
+            self.stream.close()
+            self.running = False
+            print("Closing RTD cam")
+
+
+class DeviceStreams:
+    streams: Dict[str, Any]
+    task_dev_kw: Dict[str, str]
+
+    def __init__(self):
+        self.streams = {}
+        self.task_devs_kw = None
+
+    def get_streams_by_name(self, name: str) -> List[Any]:
+        return [stream for stream_name, stream in self.streams.items() if (name in stream_name)]
+
+    def prepare(self, collection_id: str, database_name: str) -> None:
+        conn = meta.get_conn(database=database_name)
+        self.task_devs_kw = meta._get_device_kwargs_by_task(collection_id, conn)
+        if len(self.streams):
+            # print("Checking prepared devices")
+            self.streams = reconnect_streams(self.streams)
+        else:
+            self.streams = start_lsl_threads("acquisition", collection_id, conn=conn)
+
+    def record_start_cameras(self, file_name: str, task: str) -> None:
+        for name, stream in self.streams.items():
+            if name.split("_")[0] in ["hiFeed", "FLIR", "Intel", "IPhone"]:
+                if self.task_devs_kw[task].get(name):
+                    try:
+                        stream.start(file_name)
+                    except:
+                        continue
+
+    def record_start_mbients(self) -> None:
+        for name, stream in self.streams.items():
+            if "Mbient" in name:
+                try:
+                    if not stream.device.is_connected:
+                        stream.try_reconnect()
+                except Exception as e:
+                    print(e)
+                    pass
+
+    def record_stop_cameras(self, task: str) -> None:
+        for name, stream in self.streams.items():
+            if name.split("_")[0] in ["hiFeed", "FLIR", "Intel", "IPhone"]:
+                if self.task_devs_kw[task].get(name):
+                    stream.stop()
+
+    def close(self):
+        self.streams = close_streams(self.streams)
+
+
+def prepare(device_streams: DeviceStreams, message: str) -> str:
+    # Parse message
+    # format: "prepare:collection_id:database:str(log_task_dict)"
+    # TODO: Standardize and move message logic; get rid of eval
+    collection_id = message.split(':')[1]
+    database_name = message.split(':')[2]
+    log_task = message.replace(f'prepare:{collection_id}:{database_name}:', '')
+    log_task = eval(log_task)
+    subject_id_date = log_task['subject_id-date']
+
+    # Create session folder
+    ses_folder = f"{config.paths['data_out']}{subject_id_date}"
+    if not os.path.exists(ses_folder):
+        os.mkdir(ses_folder)
+
+    # Prepare device streams
+    device_streams.prepare(collection_id, database_name)
+    print("UPDATOR:-Connect-")
+    return subject_id_date
+
+
+def frame_preview(device_streams: DeviceStreams, connx) -> None:
+    steams = device_streams.get_streams_by_name('IPhone')
+    if len(steams) == 0:
+        print("no iPhone")
+        connx.send("ERROR: No iPhone in LSL streams".encode("ascii"))
+        return
+
+    # Capture frame from iPhone and send to CTL
+    frame = steams[0].frame_preview()
+    frame_prefix = b"::BYTES::" + str(len(frame)).encode("utf-8") + b"::"
+    frame = frame_prefix + frame
+    connx.send(frame)
+
+
+def record_start(device_streams: DeviceStreams, subject_id_date: str, message: str, connx) -> str:
+    print("Starting recording")
+    t0 = time()
+
+    # Parse message
+    # format: "record_start::filename::task_id" FILENAME = {subj_id}_{obs_id}
+    # TODO: Standardize and move message logic
+    file_name, task = message.split("::")[1:]
+    file_name = f"{config.paths['data_out']}{subject_id_date}/{file_name}"
+
+    device_streams.record_start_cameras(file_name, task)
+    device_streams.record_start_mbients()
+
+    print(f"Device start took {time() - t0:.2f}")
+    connx.send("ACQ_devices_ready".encode("ascii"))
+    return task
+
+
+def record_stop(device_streams: DeviceStreams, task: str, connx) -> None:
+    t0 = time()
+
+    device_streams.record_stop_cameras(task)
+
+    print(f"Device stop took {time() - t0:.2f}")
+    connx.send("ACQ_devices_stoped".encode("ascii"))
 
 
 Main()
