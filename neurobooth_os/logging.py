24a351dfe2c36b2d8920ddd00769a97b349d4c9e
Brandon Oubre <boubre@mgh.harvard.edu>
2023-05-18 17:15:42 -0400

    Yet another logging fix - fixing problem in log file names that caused error

--- neurobooth_os/logging.py
+++ neurobooth_os/logging.py
@@ -7,7 +7,7 @@ def make_session_logger(session_folder: str, machine_name: str, log_level=loggin
     formatter = logging.Formatter('|%(levelname)s| [%(asctime)s] %(filename)s, %(funcName)s, L%(lineno)d> %(message)s')
 
     logger = logging.getLogger('session')
-    time_str = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    time_str = datetime.now().strftime("%Y-%m-%d_%Hh-%Mm-%Ss")
     file_handler = logging.FileHandler(os.path.join(session_folder, f'{machine_name}_session_{time_str}.log'))
     file_handler.setLevel(log_level)
     file_handler.setFormatter(formatter)
