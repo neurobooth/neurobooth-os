ddeae2f7a496940ac5cd658804b4f5b3d8578cbd
Brandon Oubre <boubre@mgh.harvard.edu>
2023-05-18 17:10:57 -0400

    Adding timestamp to log files so that robocopy does not overwrite. Removing debug line from iphone class that was crowding the log

--- neurobooth_os/logging.py
+++ neurobooth_os/logging.py
@@ -1,12 +1,14 @@
 import os
 import logging
+from datetime import datetime
 
 
 def make_session_logger(session_folder: str, machine_name: str, log_level=logging.DEBUG) -> logging.Logger:
     formatter = logging.Formatter('|%(levelname)s| [%(asctime)s] %(filename)s, %(funcName)s, L%(lineno)d> %(message)s')
 
     logger = logging.getLogger('session')
-    file_handler = logging.FileHandler(os.path.join(session_folder, f'{machine_name}_session.log'))
+    time_str = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    file_handler = logging.FileHandler(os.path.join(session_folder, f'{machine_name}_session_{time_str}.log'))
     file_handler.setLevel(log_level)
     file_handler.setFormatter(formatter)
     logger.addHandler(file_handler)
