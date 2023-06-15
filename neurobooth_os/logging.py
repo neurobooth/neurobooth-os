db7e7fe3c8ba32513a59926fefc9c3b7f06a6239
Brandon Oubre <boubre@mgh.harvard.edu>
2023-06-15 17:11:08 -0400

    Attempt to add additional log info to dump log

--- neurobooth_os/logging.py
+++ neurobooth_os/logging.py
@@ -21,7 +21,7 @@ def make_session_logger(session_folder: str, machine_name: str, log_level=loggin
 
 def make_session_logger_debug(
         file: Optional[str] = None,
-        console: bool = True,
+        console: bool = False,
         log_level=logging.DEBUG
 ) -> logging.Logger:
     logger = logging.getLogger('session')
@@ -51,8 +51,9 @@ def make_iphone_dump_logger(
 
     logger = logging.getLogger('iphone_dump')
     time_str = datetime.now().strftime("%Y-%m-%d_%Hh-%Mm-%Ss")
+    file = os.path.join(log_path, f'iphone_dump_{time_str}.log')
 
-    file_handler = logging.FileHandler(os.path.join(log_path, f'iphone_dump_{time_str}.log'))
+    file_handler = logging.FileHandler(file)
     file_handler.setLevel(log_level)
     file_handler.setFormatter(LOG_FORMAT)
     logger.addHandler(file_handler)
@@ -61,6 +62,8 @@ def make_iphone_dump_logger(
     console_handler.setLevel(log_level)
     console_handler.setFormatter(LOG_FORMAT)
     logger.addHandler(console_handler)
+
+    make_session_logger_debug(file=file)
 
     logger.setLevel(log_level)
     return logger
