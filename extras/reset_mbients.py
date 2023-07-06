a20e802ad24c6c0ad4ddb062cd07ea82d4de2074
Brandon Oubre <boubre@mgh.harvard.edu>
2023-07-06 14:13:22 -0400

    Cleaning/refactoring reset mbient script and adding ability to specify devices via json file

--- extras/reset_mbients.py
+++ extras/reset_mbients.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
 import sys
 import argparse
+import json
+from typing import Dict, List
 from mbientlab.metawear import MetaWear, libmetawear
 from time import sleep, time
 import threading
@@ -9,21 +12,52 @@ from neurobooth_os.iout.mbient import scan_BLE
 from neurobooth_os.logging import make_default_logger
 
 
-def parse_arguments() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description='Find and reset Mbient wearable devices.')
+DESCRIPTION = """Find and reset Mbient wearable devices.
 
-    group = parser.add_argument_group(title='Scanning Arguments')
+This script has three modes of device discovery:
+    1. Scan: This is the default mode of discovery. The script will scan for nearby BLE devices until it either reaches
+    the timeout or finds the specified number of devices with the Metawear GATT service UUID.
+    Example (scan for either 10 sec or until 5 devices are found):
+        python reset_mbients.py --scan-timeout=10 --n-devices=5
+    
+    2. MAC via command line: MAC addresses can be specified via the command line using the --mac argument. The argument
+    can be specified multiple times to reset multiple devices.
+    Example (reset the devices with MAC addresses E8:95:D6:F7:39:D2 and FE:07:3E:37:F5:9C):
+        python reset_mbients.py --mac=E8:95:D6:F7:39:D2 --mac=FE:07:3E:37:F5:9C
+        
+    3. MAC va JSON file: This is simular to discovery mode 2, except it allows each MAC address to be associated with a
+    device name. The JSON file should be a dictionary where each key is a device name and each value is the
+    corresponding MAC Address.
+    Example (reset the devices in device.json):
+        python reset_mbients.py --json=device.json
+        
+        >>>contents of device.json
+        {
+        "Mbient_LH_2": "E8:95:D6:F7:39:D2",
+        "Mbient_RH_2": "FE:07:3E:37:F5:9C",
+        "Mbient_RF_2": "E5:F6:FB:6D:11:8A",
+        "Mbient_LF_2": "DA:B0:96:E4:7F:A3",
+        "Mbient_BK_1": "D7:B0:7E:C2:A1:23"
+        }
+        <<<
+"""
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+
+    group = parser.add_argument_group(title='Device Discovery')
     group.add_argument(
         '--n-devices',
         default=5,
         type=int,
-        help='The number of expected devices'
+        help='The number of expected devices when scanning. (I.e., stop scanning once this many devices are found.)'
     )
     group.add_argument(
         '--scan-timeout',
         default=10,
         type=int,
-        help='Specify a timeout (in seconds) for identifying the devices.'
+        help='Specify a timeout (in seconds) for scanning for devices.'
     )
     group.add_argument(
         '--mac',
@@ -33,6 +67,13 @@ def parse_arguments() -> argparse.Namespace:
         type=str,
         help='Instead of scanning for devices, specify the MAC address. '
              'This argument can be specified multiple times to reset multiple devices.'
+    )
+    group.add_argument(
+        '--json',
+        default=None,
+        type=str,
+        help='Instead of scanning for devices, specify a JSON file containing a dictionary '
+             'of DEVICE_NAME: MAC_ADDRESS pairs.'
     )
 
     group = parser.add_argument_group(title='Reset Arguments')
@@ -64,21 +105,26 @@ def parse_arguments() -> argparse.Namespace:
 
 
 class ResetDeviceThread(threading.Thread):
-    def __init__(self, address: str, connect_attempts: int, reset_timeout: float):
+    def __init__(self, address: str, name: str, connect_attempts: int, reset_timeout: float):
         """
         When started, this thread will try to connect to and reset the specified Mbient device.
 
         :param address: The MAC address of the device to reset.
+        :param name: The device's name (for logging).
         :param connect_attempts: The number of times to try to connect to the device before giving up.
         :param reset_timeout: How long to wait for the device to reset before giving up.
         """
         super().__init__()
         self.address = address
+        self.name = name
         self.connect_attempts = connect_attempts
         self.reset_timeout = reset_timeout
         self.logger = logging.getLogger('default')
         self.disconnect_event = threading.Event()
         self.success = False
+
+    def format_message(self, msg: str) -> str:
+        return f'{self.name} [{self.address}]: {msg}'
 
     def run(self) -> None:
         t0 = time()
@@ -90,9 +136,9 @@ class ResetDeviceThread(threading.Thread):
             self.logger.exception(e)
         finally:
             if self.success:
-                self.logger.debug(f'Reset for {self.address} took {time() - t0:0.1f} sec.')
+                self.logger.debug(self.format_message(f'Reset took {time() - t0:0.1f} sec.'))
             else:
-                self.logger.debug(f'Reset timeout reached for {self.address}')
+                self.logger.debug(self.format_message(f'Reset timed out!'))
 
     def connect(self) -> MetaWear:
         """
@@ -103,7 +149,7 @@ class ResetDeviceThread(threading.Thread):
         device = MetaWear(self.address)
 
         success = False
-        self.logger.debug(f'Attempting connection to {self.address}')
+        self.logger.debug(self.format_message(f'Attempting Connection'))
         for i in range(self.connect_attempts):
             try:
                 if i > 0:  # Do not immediately try to reconnect if it just failed.
@@ -113,16 +159,16 @@ class ResetDeviceThread(threading.Thread):
                 success = True
                 break
             except Exception as e:
-                self.logger.debug(f'Failed to connect to {self.address} on attempt {i+1}: {e}')
+                self.logger.debug(self.format_message(f'Failed to connect on attempt {i + 1}: {e}'))
 
         if not success:
-            raise Exception(f'Unable to connect to {self.address}')
+            raise Exception(self.format_message(f'Unable to connect!'))
 
         device.on_disconnect = lambda status: self.on_disconnect(status)
         return device
 
     def on_disconnect(self, status) -> None:
-        self.logger.debug(f'Disconnected {self.address} with status {status}.')
+        self.logger.debug(self.format_message(f'Disconnected with status {status}.'))
         self.disconnect_event.set()
 
     @staticmethod
@@ -142,34 +188,83 @@ class ResetDeviceThread(threading.Thread):
         libmetawear.mbl_mw_debug_disconnect(board)
 
 
-def main():
-    logger = make_default_logger()
-    args = parse_arguments()
+ADDRESS_MAP = Dict[str, str]  # Type alias for return of discovery functions
 
-    if args.mac:
-        devices = {f'CL-{i}': address for i, address in enumerate(args.mac)}
-        logger.debug(f'Using {len(devices)} devices specified via command line: {devices}')
+
+class DeviceDiscoveryException(Exception):
+    pass
+
+
+def device_discovery(args: argparse.Namespace) -> ADDRESS_MAP:
+    if args.json is not None:
+        devices = discovery_json(args.json)
+    elif args.mac:
+        devices = discovery_mac(args.mac)
     else:
-        t0 = time()
-        logger.info('Scanning for devices...')
-        devices = scan_BLE(timeout_sec=args.scan_timeout, n_devices=args.n_devices)
-        logger.info(f'Identified {len(devices)} devices. Scan took {time() - t0:0.1f} sec.')
-        if len(devices) == 0:
-            logger.error('No devices found!')
-            return
-        elif len(devices) < args.n_devices:
-            logger.warning('Not all devices found!')
+        devices = discovery_scan(timeout_sec=args.scan_timeout, n_devices=args.n_devices)
 
+    logger = logging.getLogger('default')
     for k, v in devices.items():
         logger.debug(f'Device {k} Address: {v}')
 
+    return devices
+
+
+def discovery_scan(timeout_sec: float, n_devices: int) -> ADDRESS_MAP:
+    logger = logging.getLogger('default')
+    logger.info('Scanning for devices...')
+
+    t0 = time()
+    devices = scan_BLE(timeout_sec=timeout_sec, n_devices=n_devices)
+    logger.info(f'Scan took {time() - t0:0.1f} sec.')
+
+    if len(devices) == 0:
+        raise DeviceDiscoveryException('No devices found!')
+    elif len(devices) < n_devices:
+        logger.warning(f'Only {len(devices)} of {n_devices} devices found!')
+    else:
+        logger.info(f'Scan identified {len(devices)} devices.')
+
+    return devices
+
+
+def discovery_mac(addresses: List[str]) -> ADDRESS_MAP:
+    logger = logging.getLogger('default')
+    devices = {f'CL-{i}': address for i, address in enumerate(addresses)}
+    logger.info(f'Using {len(devices)} devices specified via command line.')
+    return devices
+
+
+def discovery_json(file: str) -> ADDRESS_MAP:
+    logger = logging.getLogger('default')
+
+    with open(file, 'r') as f:
+        devices = json.load(f)
+
+    # File validation checks
+    if not isinstance(devices, dict):
+        raise DeviceDiscoveryException(f'{file} should contain a dictionary mapping device names to MAC addresses.')
+    if len(devices) == 0:
+        raise DeviceDiscoveryException(f'No devices specified in {file}!')
+    for k, v in devices.items():
+        if not isinstance(k, str) or not isinstance(v, str):
+            raise DeviceDiscoveryException(f'{file} should contain a dictionary mapping device names to MAC addresses.')
+
+    logger.info(f'Using {len(devices)} devices specified in: {file}')
+    return devices
+
+
+def reset_devices(args: argparse.Namespace, devices: ADDRESS_MAP) -> None:
     reset_threads = [
         ResetDeviceThread(
             address=address,
+            name=name,
             connect_attempts=args.n_connect_attempts,
             reset_timeout=args.reset_timeout,
-        ) for _, address in devices.items()
+        ) for name, address in devices.items()
     ]
+
+    logger = logging.getLogger('default')
     logger.info(f'Reset in progress...')
     t0 = time()
     for t in reset_threads:
@@ -185,11 +280,20 @@ def main():
         logger.warning(f'Failed to reset {len(failure)} devices: {failure}')
 
 
-if __name__ == "__main__":
+def main():
+    logger = make_default_logger()
     try:
-        main()
+        args = parse_arguments()
+        devices = device_discovery(args)
+        reset_devices(args, devices)
+    except DeviceDiscoveryException as e:
+        logger.exception(e)
+        raise e
     except Exception as e:
-        logger = logging.getLogger('default')
         logger.critical(f"An uncaught exception occurred. Exiting: {repr(e)}")
         logger.critical(e, exc_info=sys.exc_info())
         raise e
+
+
+if __name__ == "__main__":
+    main()
