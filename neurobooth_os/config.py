e2c7c86c18b52768e36ff9bacf595d3afeea0dcc
Larry White <ljw1001@gmail.com>
2023-07-18 19:15:33 -0400

    Update config.py

--- neurobooth_os/config.py
+++ neurobooth_os/config.py
@@ -8,24 +8,35 @@ import json
 
 from neurobooth_os.util.constants import NODE_NAMES
 
-fname = path.join(environ.get("NB_CONFIG"), "neurobooth_os_config.json")
 
-if not path.exists(fname):
-    msg = "Required config file does not exist"
-    raise IOError(msg)
+def validate_folder(value):
+    if not path.exists(value):
+        raise FileNotFoundError(f"The folder ({value}) does not exist.")
+    if not path.isdir(value):
+        raise IOError(f"The path ({value}) is not a folder.")
 
-with open(fname, "r") as f:
-    neurobooth_config = json.load(f)
 
-    destination = neurobooth_config["remote_data_dir"]
-    if not path.exists(destination):
-        raise FileNotFoundError(f"The remote_data_dir ({destination}) does not exist.")
-    if not path.isdir(destination):
-        raise IOError(f"The remote_data_dir ({destination}) is not a folder.")
+def main():
+    fname = path.join(environ.get("NB_CONFIG"), "neurobooth_os_config.json")
 
-    for name in NODE_NAMES:
-        source = neurobooth_config[name]["local_data_dir"]
-        if not path.exists(source):
-            raise FileNotFoundError(f"The local_data_dir ({source}) for server {name} does not exist.")
-        if not path.isdir(source):
-            raise IOError(f"The local_data_dir ({source}) for server {name} is not a folder.")
+    if not path.exists(fname):
+        msg = "Required config file does not exist"
+        raise IOError(msg)
+
+    with open(fname, "r") as f:
+        neurobooth_config = json.load(f)
+
+        validate_folder(neurobooth_config["remote_data_dir"])
+        validate_folder(neurobooth_config["video_tasks"])
+        validate_folder(neurobooth_config["default_log_path"])
+    
+        for name in NODE_NAMES:
+            source = neurobooth_config[name]["local_data_dir"]
+            if not path.exists(source):
+                raise FileNotFoundError(f"The local_data_dir ({source}) for server {name} does not exist.")
+            if not path.isdir(source):
+                raise IOError(f"The local_data_dir ({source}) for server {name} is not a folder.")
+
+
+if __name__ == "__main__":
+    main()
