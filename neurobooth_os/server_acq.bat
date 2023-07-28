beebde588d97a116df7bed0f2695b5f8e85c88d6
Larry White <ljw1001@gmail.com>
2023-07-28 11:04:50 -0400

    fix command line args for server start scripts (#297)

--- neurobooth_os/server_acq.bat
+++ neurobooth_os/server_acq.bat
@@ -1,4 +1,4 @@
 call C:\Users\ACQ\anaconda3\Scripts\activate.bat %NB_CONDA%
 start /W ipython --pdb %NB_INSTALL%\neurobooth_os\server_acq.py
 start /W ipython %NB_INSTALL%\extras\dump_iphone_video.py
-start /W ipython %NB_INSTALL%\neurobooth_os\transfer_data.py ACQ
+start /W ipython %NB_INSTALL%\neurobooth_os\transfer_data.py acquisition
