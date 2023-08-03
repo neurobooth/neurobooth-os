af4149b4484098e488e0bbad01e200fcf38e0b72
Brandon Oubre <boubre@mgh.harvard.edu>
2023-08-03 13:54:40 -0400

    Functionless prototype of reset task

---
+++ neurobooth_os/tasks/mbient_reset.py
@@ -0,0 +1,67 @@
+from typing import Optional, Dict
+from enum import IntEnum, auto
+from neurobooth_os.tasks.task import Task
+from neurobooth_os.tasks.utils import get_keys
+from psychopy import visual
+from neurobooth_os.iout.mbient import Mbient
+
+
+class UserInputEvent(IntEnum):
+    CONTINUE = auto()
+    RESET = auto()
+
+
+class MbientResetPauseError(Exception):
+    pass
+
+
+class MbientResetPause(Task):
+    """Pause the session so that the Mbient wearables can be reset to improve data quality."""
+
+    def __init__(
+            self,
+            mbients: Optional[Dict[str, Mbient]] = None,
+            continue_key: str = 'return',
+            reset_key: str = 'r',
+            **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.mbients = mbients
+        self.continue_key = continue_key
+        self.reset_key = reset_key
+
+        self._screen = visual.TextStim(
+            self.win,
+            "Please wait while we reset the wearable devices.",
+            height=32,
+            color=[1, 1, 1],
+            pos=(0, 32),
+            units="pix",
+        )
+
+    def run(self, **kwargs):
+        # Present Intro Screen
+        self._screen.draw()
+        self.win.flip()
+
+        try:  # Perform resets until the continue key is pressed
+            event = self.wait_for_key()
+            while event == UserInputEvent.RESET:
+                self.reset_mbients()
+                event = self.wait_for_key()
+        except MbientResetPauseError as e:
+            self.logger.exception(e)
+
+        # Clean Up
+
+    def wait_for_key(self) -> UserInputEvent:
+        key = get_keys(keyList=[self.continue_key, self.reset_key])
+        if key == self.continue_key:
+            return UserInputEvent.CONTINUE
+        elif key == self.reset_key:
+            return UserInputEvent.RESET
+        else:
+            raise MbientResetPauseError(f'Reached "impossible" case with key={key}')
+
+    def reset_mbients(self) -> None:
+        pass
