32b12357c7328901ee877530733c42db5b5e7447
Siddharth Patel <36140839+sid4py@users.noreply.github.com>
2023-08-14 15:18:46 -0400

    run_xdf_split_postproces reads database name from config now, split xdf replaces backslashes with forward slashes for replace to work with config (#302)
    Co-authored-by: neurobooth-bot <neuroboothteam@gmail.com>

--- extras/run_xdf_split_postproces.py
+++ extras/run_xdf_split_postproces.py
@@ -1,7 +1,8 @@
 from neurobooth_os.iout.metadator import get_conn
 from neurobooth_os.iout.split_xdf import create_h5_from_csv
+from neurobooth_os.config import neurobooth_config
 
-conn = get_conn()
+conn = get_conn(neurobooth_config['database']['dbname'])
 # location of the csv file containing path filename and task ID
 dont_split_xdf_fpath = "C:/neurobooth"
 create_h5_from_csv(dont_split_xdf_fpath, conn)
