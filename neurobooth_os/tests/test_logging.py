a7a9bc4332ebe1396b3898d3eca6327d5a16b59e
Larry White <ljw1001@gmail.com>
2023-10-02 12:03:46 -0400

    Additional work on test automation involving the database

--- neurobooth_os/tests/test_logging.py
+++ neurobooth_os/tests/test_logging.py
@@ -1,12 +1,40 @@
+import inspect
 import logging
 import os
 import shutil
 import sys
 import unittest
 
+from neurobooth_terra import Table
+
 from neurobooth_os.log_manager import make_default_logger, make_db_logger
+from neurobooth_os.iout.metadator import get_conn
 
 log_path = r"C:\neurobooth\test_data\test_logs"
+database = "mock_neurobooth_1"
+
+subject = "1111111"
+session = "1111111_2023_12_25 12:12:12"
+
+
+def get_records(conn=get_conn(database), where=None):
+    """Test utility for querying log_application table. Returns results as a dataframe"""
+    table = Table("log_application", conn=conn)
+    if where is not None:
+        task_df = table.query(where)
+    else:
+        task_df = table.query()
+    print(task_df)
+    return task_df
+
+
+def delete_records(conn=get_conn(database), where=None) -> None:
+    """Test utility for querying log_application table. Returns results as a dataframe"""
+    table = Table("log_application", conn=conn)
+    if where is not None:
+        table.delete_row(where)
+    else:
+        table.delete_row("1=1")
 
 
 class TestLogging(unittest.TestCase):
@@ -22,6 +50,7 @@ class TestLogging(unittest.TestCase):
             logger.removeHandler(handler)
             handler.close()
         logging.shutdown()
+        delete_records()
 
         if os.path.exists(log_path):
             files = os.listdir(log_path)
@@ -52,7 +81,7 @@ class TestLogging(unittest.TestCase):
         self.assertTrue("Traceback" in data)
 
     def test_db_logging_shutdown(self):
-        """Tests to ensure log handler is closed """
+        """Tests to ensure log handler is closed (or at least, doesn't blow up when closing) """
         db_log = make_db_logger("1111111", "1111111_2023_12_25 12:12:12")
         db_log.critical("Microphone: Entering LSL Loop", extra={"device": "playstation"})
         logging.shutdown()
@@ -63,17 +92,51 @@ class TestLogging(unittest.TestCase):
         db_log.critical("Microphone: Entering LSL Loop", extra={"device": "playstation"})
         db_log.critical("Another one.", extra={"device": "playstation"})
 
+        df = get_records()
+        assert df.iloc[0]["subject_id"] == subject
+        assert df.iloc[0]["session_id"] == session
+        assert df.iloc[0]["message"] == "Microphone: Entering LSL Loop"
+
+        assert df.iloc[1]["subject_id"] == subject
+        assert df.iloc[1]["session_id"] == session
+        assert df.iloc[1]["message"] == "Another one."
+
     def test_db_logging1(self):
         """Tests logging to database using make_db_logger with session and subject set to empty strings"""
         db_log = make_db_logger("", "")
-        db_log.critical("No subject or session when log created.", extra={"device": "playstation"})
+        msg = "No subject or session when log created."
+        db_log.critical(msg, extra={"device": "playstation"})
+
+        mod_name = sys.modules[__name__].__name__ + ".py"
+        fun_name = inspect.stack()[0][3]
+
+        df = get_records()
+        assert df.shape[0] == 1
+        assert df.iloc[0]["log_level"].upper() == "CRITICAL"
+        assert df.iloc[0]["device"] == "playstation"
+        assert df.iloc[0]["subject_id"] == ""
+        assert df.iloc[0]["session_id"] == ""
+        assert df.iloc[0]["server_type"] == "control"
+        assert df.iloc[0]["server_id"] != ""
+        assert df.iloc[0]["server_time"] is not None
+        assert df.iloc[0]["filename"] == mod_name
+        assert df.iloc[0]["function"] == fun_name
+        assert df.iloc[0]["line_no"] >= 1
+        assert df.iloc[0]["message"] == msg
+        assert df.iloc[0]["traceback"] is None
 
     def test_db_logging2(self):
         """Tests logging to database with session and subject set to empty strings AFTER being set to values"""
-        db_log = make_db_logger("1111111", "1111111_2023_12_25 12:12:12")
+        db_log = make_db_logger(subject, session)
         db_log.critical("Subject and session set for the logger", extra={"device": "playstation"})
         db_log = make_db_logger("", "")
         db_log.error("No subject or session for new records")
+        df = get_records()
+        assert df.iloc[0]["subject_id"] == subject
+        assert df.iloc[0]["session_id"] == session
+
+        assert df.iloc[1]["subject_id"] == ""
+        assert df.iloc[1]["session_id"] == ""
 
     # @unittest.skip("Test requires that DB Connection not succeed (i.e. run outside VPN)")
     def test_db_logging3(self):
@@ -96,6 +159,17 @@ class TestLogging(unittest.TestCase):
             raise Exception("This is a test")
         except Exception as Argument:
             db_log.exception("Test db logging with traceback")
+        df = get_records()
+        assert df.iloc[0]["traceback"] is not None
+        assert "This is a test" in df.iloc[0]["traceback"]
+
+    def test_get_records(self):
+        """Meta-testing: Tests the get_records and delete_records utility function used in these tests"""
+        df = get_records()
+        assert(df is not None)
+        delete_records()
+        df = get_records()
+        assert df.empty
 
 
 if __name__ == '__main__':
