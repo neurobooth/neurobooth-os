2d14b797a73e2593b8fbc1fc491a699ff2ebeb44
Larry White <ljw1001@gmail.com>
2024-01-31 11:47:06 -0500

    Revert "remove unused device code"
    This reverts commit b5c469d705c10e958d0fb29d8260d3d8df894750.

---
+++ neurobooth_os/iout/gato_lights.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 18 14:46:18 2021
+
+@author: STM
+"""
+
+import leglight
+
+myLight_L = leglight.LegLight("192.168.137.81", 9123)
+myLight_L.on()
+myLight_L.color(5500)
+
+myLight_R = leglight.LegLight("192.168.137.156", 9123)
+myLight_R.on()
+myLight_R.color(5500)
+
+
+# allLights = leglight.discover(2)
+# allLights.on()
+# # >>> myLight.brightness(14)
+# # >>> myLight.color(3500)
+# # >>> myLight.off()
+
+myLight_L.off()
+myLight_R.off()
