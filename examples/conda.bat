3015171ff967785300f91910d3f810f0eca364f0
Larry White <ljw1001@gmail.com>
2024-02-02 14:30:59 -0500

    Create conda.bat
    script for creating a neurobooth environment in conda.   Experimental

---
+++ examples/conda.bat
@@ -0,0 +1,7 @@
+conda deactivate
+conda env remove --name neurobooth-staging
+conda env create --file environment_staging.yml
+pip install -e .
+pip install --index-url=https://pypi.sr-support.com sr-research-pylink
+pip install c:\spinnaker\spinnaker_python-3.1.0.79-cp38-cp38-win_amd64.whl
+pip install --user --force-reinstall h5py-3.10.0-cp312-cp312-win_amd64.whl
