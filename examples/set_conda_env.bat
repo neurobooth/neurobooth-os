82319de8372c189de97d810c0f11d24b3fc33b9d
Larry White <ljw1001@gmail.com>
2024-02-05 11:20:04 -0500

    trying removing the pytables dependency

--- examples/set_conda_env.bat
+++ examples/set_conda_env.bat
@@ -11,4 +11,9 @@ call pip install -e .
 call pip install --index-url=https://pypi.sr-support.com sr-research-pylink
 call pip install c:\spinnaker\spinnaker_python-3.1.0.79-cp38-cp38-win_amd64.whl
 pause
-call pip install --user --force-reinstall h5py==3.7.0
+call conda remove hdf5
+pip uninstall h5py
+pip uninstall h5io
+pip install h5py==3.7.0
+pip install h5io
+:: call pip install --user --force-reinstall h5py==3.7.0
