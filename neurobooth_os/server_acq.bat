53da108af1d5157ed06bafdb1aa19401bef1c765
Brandon Oubre <boubre@mgh.harvard.edu>
2024-02-09 13:39:56 -0500

    Disabling post-session iphone dump because of workflow issues during busy collection days. (#373)

--- neurobooth_os/server_acq.bat
+++ neurobooth_os/server_acq.bat
@@ -1,4 +1,3 @@
 call %NB_CONDA_INSTALL%\Scripts\activate.bat %NB_CONDA_ENV%
 start /W ipython --pdb %NB_INSTALL%\neurobooth_os\server_acq.py
-start /W ipython %NB_INSTALL%\extras\dump_iphone_video.py
 start /W ipython %NB_INSTALL%\neurobooth_os\transfer_data.py acquisition
