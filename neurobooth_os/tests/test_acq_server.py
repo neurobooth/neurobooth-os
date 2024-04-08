7d89074696e2cf538230d670e6866712e9ba774b
Larry White <ljw1001@gmail.com>
2024-04-08 10:40:33 -0400

    fix test

--- neurobooth_os/tests/test_acq_server.py
+++ neurobooth_os/tests/test_acq_server.py
@@ -23,7 +23,8 @@ class TestTaskParamReader(unittest.TestCase):
 
     def test_get_dev_kwargs(self):
         collection_id = 'testing'
-        devkwargs = DeviceManager._get_unique_devices(collection_id)
+        task_params = meta.build_tasks_for_collection(collection_id)
+        devkwargs = DeviceManager._get_unique_devices(collection_id, task_params)
         print(devkwargs)
 
     def test_start_flir(self):
