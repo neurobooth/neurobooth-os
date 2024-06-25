727ed0a4e84b24244bac44fc6ed5dd2dcbb8871f
Larry White <ljw1001@gmail.com>
2024-06-25 11:50:33 -0400

    undo inadvertent change to mbient hdf5 script

--- extras/mbient_hdf5_inspect.py
+++ extras/mbient_hdf5_inspect.py
@@ -10,9 +10,9 @@ from h5io import read_hdf5
 import os.path as op
 import numpy as np
 
-lsl_session = "100089_2022-06-01"
-lsl_session = "100064_2022-06-03"
-files = glob.glob(f"Z:/data/{lsl_session}/{lsl_session}*Mbient*.hdf5")
+session = "100089_2022-06-01"
+session = "100064_2022-06-03"
+files = glob.glob(f"Z:/data/{session}/{session}*Mbient*.hdf5")
 
 for fl in files:
     data = read_hdf5(fl)
@@ -21,7 +21,7 @@ for fl in files:
 
     _, name = op.split(fl)
     print(
-        name.replace(lsl_session, "").replace("-mbient_acc_1-mbient_gyro_1.hdf5", ""),
+        name.replace(session, "").replace("-mbient_acc_1-mbient_gyro_1.hdf5", ""),
         len(fps),
         1 / np.mean(np.diff(fps)),
     )
