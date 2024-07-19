90691ce87383e1936960926d290f537c861a6632
Brandon Oubre <boubre@mgh.harvard.edu>
2024-07-19 08:33:48 -0400

    Revising script skeleton based on conversations that we will look to the database log tables for device ids

--- extras/split_xdf.py
+++ extras/split_xdf.py
@@ -6,7 +6,8 @@ installation of Neurobooth-OS.
 import os
 import re
 import argparse
-from typing import Optional, Dict, Any
+import datetime
+from typing import Dict, NamedTuple
 
 import neurobooth_os.iout.split_xdf as xdf
 
@@ -16,38 +17,55 @@ class SplitException(Exception):
     pass
 
 
-XDF_NAME_PATTERN = re.compile(r'\d+_\d\d\d\d-\d\d-\d\d_\d\dh-\d\dm-\d\ds_(.*)_R001\.xdf', flags=re.IGNORECASE)
+XDF_NAME_PATTERN = re.compile(r'(\d+)_(\d\d\d\d-\d\d-\d\d)_\d\dh-\d\dm-\d\ds_(.*)_R001\.xdf', flags=re.IGNORECASE)
 
 
-def infer_task_id(xdf_path: str) -> str:
-    """
-    Attempt to infer the task ID from the XDF file name.
-    :param xdf_path: The path to the XDF file.
-    :return: The inferred task ID. Raises a SplitException if the filename could not be parsed.
-    """
-    _, filename = os.path.split(xdf_path)
-    match = re.match(XDF_NAME_PATTERN, filename)
-    if match is None:
-        raise SplitException(f'Unable to determine task ID from file name: {filename}')
-    return match.group(1)
+class XDFInfo(NamedTuple):
+    """Structured representation of an XDF file name."""
+    parent_dir: str
+    name: str
+    subject_id: str
+    date: datetime.date
+    task_id: str
+
+    @property
+    def path(self) -> str:
+        return os.path.join(self.parent_dir, self.name)
+
+    @staticmethod
+    def parse_xdf_name(xdf_path: str) -> 'XDFInfo':
+        """
+        Attempt to infer the subject ID, date, and task ID from the XDF file path.
+        :param xdf_path: The path to the XDF file.
+        :return: A structured representation of the XDF file name.
+        """
+        parent_dir, filename = os.path.split(xdf_path)
+        match = re.match(XDF_NAME_PATTERN, filename)
+        if match is None:
+            raise SplitException(f'Unable to parse file name: {filename}')
+
+        subject_id, date_str, task_id = match.groups()
+        return XDFInfo(
+            parent_dir=parent_dir,
+            name=filename,
+            subject_id=subject_id,
+            date=datetime.date.fromisoformat(date_str),
+            task_id=task_id,
+        )
 
 
 def split(
         xdf_path: str,
-        task_config: str,
-        task_id: Optional[str] = None,
+        config_path: str,
 ) -> None:
     """
     Split a single XDF file into device-specific HDF5 files.
     Intended to be called either via the command line (via parse_arguments()) or by another script (e.g., one that
     finds and iterates over all files to be split).
     :param xdf_path: The path to the XDF file to split.
-    :param task_config: The path to task configuration definitions.
-    :param task_id: The task ID string (or None to attempt to infer it from the file name).
+    :param config_path: The path to a Neurobooth configuration file with a 'database' entry.
     """
-    if task_id is None:
-        task_id = infer_task_id(xdf_path)
-
+    xdf_info = XDFInfo.parse_xdf_name(xdf_path)
     device_ids = None  # TODO: Get device IDs for the task!
 
     device_data = xdf.parse_xdf(xdf_path, device_ids)
@@ -55,7 +73,7 @@ def split(
     xdf.write_device_hdf5(device_data)
 
 
-def parse_arguments() -> Dict[str, Any]:
+def parse_arguments() -> Dict[str, str]:
     """
     Parse command line arguments.
     :return: Dictionary of keyword arguments to split().
@@ -65,25 +83,18 @@ def parse_arguments() -> Dict[str, Any]:
         '--xdf',
         required=True,
         type=str,
-        help='Path to the XDF file to split.'
+        help="Path to the XDF file to split."
     )
     parser.add_argument(
-        '--task-config',
-        default=None,
+        '--config-path',
+        required=True,
         type=str,
-        help='Path to the Neurobooth-OS task config folder.'
-    )
-    parser.add_argument(
-        '--task-id',
-        default=None,
-        type=str,
-        help='The task ID of the file being split. (Attempt to extract from file name if not specified.)'
+        help="Path to a Neurobooth configuration file with a 'database' entry."
     )
     args = parser.parse_args()
     return {
         'xdf_path': os.path.abspath(args.xdf),
-        'task_config': os.path.abspath(args.task_config),
-        'task_id': args.task_id,
+        'config_path': os.path.abspath(args.config_path),
     }
 
 
