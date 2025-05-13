14ee9056ab45e276396534845885a791dcc9139b
Larry White <ljw1001@gmail.com>
2025-05-13 11:27:18 -0400

    Update client.py

--- neurobooth_os/netcomm/client.py
+++ neurobooth_os/netcomm/client.py
@@ -226,6 +226,8 @@ def start_server(node_name, save_pid_txt=True):
     else:
         task_cmd = "tasklist.exe"
 
+    print(f"About to run task_cmd: {task_cmd}")
+\
     out = os.popen(task_cmd).read()
     logger.debug(f"Python processes found: {out}")
     pids_old = get_python_pids(out)
