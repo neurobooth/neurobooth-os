7f4633dc4ff40c876d447bc5caec3d8cc8dfa7a3
Larry White <ljw1001@gmail.com>
2025-07-10 17:32:31 -0400

    iphone prepare does not work with None configs anymore

--- extras/dump_iphone_video.py
+++ extras/dump_iphone_video.py
@@ -35,7 +35,15 @@ def neurobooth_dump(args: argparse.Namespace) -> None:
     logger.debug('Connecting to iPhone')
     # It is important that timeout exceptions are enabled to prevent misnamed files!
     phone = iphone.IPhone("dump_iphone", enable_timeout_exceptions=True)
-    handshake_success = phone.prepare()
+    default_config: CONFIG = {
+        "NOTIFYONFRAME": "1",
+        "VIDEOQUALITY": "1920x1080",
+        "USECAMERAFACING": "BACK",
+        "FPS": "240",
+        "BRIGHTNESS": "50",
+        "LENSPOS": "0.7",
+    }
+    handshake_success = phone.prepare(config=default_config)
     if not handshake_success:
         logger.error(f'Unable to connect to iPhone [state={phone._state}]!')
         return
