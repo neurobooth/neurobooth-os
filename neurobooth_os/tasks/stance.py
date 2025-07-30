2e9f6eeedb309d442c7a1aecf86cff4a10333038
neurobooth_bot <neuroboothteam@gmail.com>
2025-07-30 16:55:49 -0400

    stance.py

--- neurobooth_os/tasks/stance.py
+++ neurobooth_os/tasks/stance.py
@@ -1,11 +1,27 @@
 from neurobooth_os.tasks.task import Task
 from neurobooth_os.tasks import utils
-
+from psychopy import sound
+from pylsl import local_clock
 
 class Stance(Task):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    def play_tone(self):
+        tone = sound.Sound(1000, 0.2, stereo=True)
+        tone.play()
+    
+    def timer(self, elapsed_time:int = 0, time_period: int = 1):
+        # start timer 
+        t1 = local_clock()
+        t2 = t1
+        while t2 - t1 < time_period:
+            t2 = local_clock()
+        # timer ends -> update elapsed time
+        return elapsed_time + time_period
+        
+
+    
     def present_task(
         self,
         prompt=True,
@@ -13,6 +29,7 @@ class Stance(Task):
         wait_keys=True,
         trial_intruct=["trial 1", "trial 2"],
         trial_text='Press any key to end trial',
+        use_timer=False,
         **kwargs
     ):
 
@@ -20,9 +37,9 @@ class Stance(Task):
 
         for nth, trl in enumerate(trial_intruct):
             self.display_trial_instructions(trl)
-            # self.countdown_to_stimulus()
-            self.perform_trial(duration, wait_keys, trial_text)
-            self.present_trial_ended_msg(trial_number=nth+1)
+            self.countdown_to_stimulus()
+            trial_time = self.perform_trial(duration, wait_keys, trial_text, use_timer)
+            self.present_trial_ended_msg(trial_number=nth+1, trial_time=trial_time)
 
         self.send_marker(self.marker_task_end)
 
@@ -37,22 +54,39 @@ class Stance(Task):
                 waitKeys=False,
             )
 
-    def perform_trial(self, duration: int, wait_keys: bool, trial_text: str):
+    def perform_trial(self, duration: int, wait_keys: bool, trial_text: str, use_timer: bool):
+        # change screen color from grey to white and flip to update the screen
         white = (1, 1, 1)
         self.win.color = white
         self.win.flip()
-        self.show_text(
-            screen=utils.create_text_screen(win=self.win, text=trial_text, text_color="black"),
-            msg="Trial",
-            wait_time=duration,
-            abort_keys=None, # self.abort_keys,
-            waitKeys=wait_keys
-        )
+
+        if use_timer: # Timed trials that end with key press - Stance tasks
+            pass
+        elif duration and wait_keys==False: # Show trial screen for set duration and end - Sitting task
+            self.show_text(
+                screen=utils.create_text_screen(win=self.win, text=trial_text, text_color="black"),
+                msg="Trial",
+                wait_time=duration,
+                waitKeys=wait_keys,
+                abort_keys=None, # Stance trials cannot be aborted
+            )
+            return duration
+        elif duration==0 and wait_keys: # Trial with unlimited duration which ends with key press - Tandem Walk
+            self.show_text(
+                screen=utils.create_text_screen(win=self.win, text=trial_text, text_color="black"),
+                msg="Trial",
+                wait_time=duration,
+                waitKeys=wait_keys,
+                abort_keys=None, # Stance trials cannot be aborted
+            )
+            return duration
+            
         
 
-    def present_trial_ended_msg(self, trial_number: int):
-        msg = utils.create_text_screen(self.win, f"Trial {trial_number} ended")
-        utils.present(self.win, msg, wait_time=2, waitKeys=False)
+    def present_trial_ended_msg(self, trial_number: int, trial_time: int):
+        trial_end_text = f"Trial {trial_number} ended\n\nTime Elapsed = {trial_time}\n\nPress CONTINUE to proceed"
+        trial_end_screen = utils.create_text_screen(self.win, trial_end_text)
+        utils.present(self.win, trial_end_screen, waitKeys=True, abort_keys=self.abort_keys)
 
 
     def display_trial_instructions(self, trl_instructions) -> None:
@@ -62,7 +96,7 @@ class Stance(Task):
         ----------
         trl_instructions str: The instructions for the current trial
         """
-        msg = utils.create_text_screen(self.win, trl_instructions + "\n\n\nPress CONTINUE to start trial")
+        msg = utils.create_text_screen(self.win, trl_instructions + "\n\nPress CONTINUE to start trial")
         utils.present(self.win, msg)
 
 
