65f35c48f7bc69c107fa0a00807a6685309260f3
houadoudibelabzioui <hasnaa.ouadoudi97@gmail.com>
2025-08-07 13:26:34 -0400

    Update flir_cam.py (#519)
    Inverted FLIR frame preview at Wang by removing line 250: rc, img = cv2.imencode('.png', img) in flir_cam.py

--- neurobooth_os/iout/flir_cam.py
+++ neurobooth_os/iout/flir_cam.py
@@ -247,7 +247,7 @@ class VidRec_Flir(CameraPreviewer):
         self.cam.EndAcquisition()
 
         # Open question: why is the image not flipped during active recording?
-        img = cv2.rotate(img, cv2.ROTATE_180)
+        # img = cv2.rotate(img, cv2.ROTATE_180)
 
         rc, img = cv2.imencode('.png', img)
         return img.tobytes() if rc else b""
