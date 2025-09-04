11ac79dc4804725cf2f064e24dac3e79ab6366c5
Siddharth Patel <36140839+sid4py@users.noreply.github.com>
2025-09-04 14:26:12 -0400

    Update postprocess_split_xdf.bat (#528)

--- extras/postprocess_split_xdf.bat
+++ extras/postprocess_split_xdf.bat
@@ -1,2 +1,2 @@
 call  %NB_CONDA_INSTALL%\Scripts\activate.bat %NB_CONDA_ENV%
-call start /W ipython C:\neurobooth-os\extras\run_xdf_split_postproces.py
+call start /W ipython %NB_INSTALL%\extras\run_xdf_split_postproces.py
