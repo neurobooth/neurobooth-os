74bdf28b4910d19ef93ada7cb9e2b54eb681a849
Larry White <ljw1001@gmail.com>
2025-10-09 13:03:30 -0400

    Update task_introduction.py

--- neurobooth_os/tasks/task_introduction.py
+++ neurobooth_os/tasks/task_introduction.py
@@ -3,7 +3,7 @@ from neurobooth_os.tasks import Task
 
 class Introduction_Task(Task):
     def __init__(self, **kwargs):
-        super().__init__(kwargs)
+        super().__init__(**kwargs)
 
     def run(self, **kwargs):
         self.present_instructions(prompt=False)
