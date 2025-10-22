7420e433e5d6d76f0040eff49581539b8c382dee
Larry White <ljw1001@gmail.com>
2025-10-22 15:34:30 -0400

    Update task_show_progress_bar.py

--- neurobooth_os/tasks/task_show_progress_bar.py
+++ neurobooth_os/tasks/task_show_progress_bar.py
@@ -8,6 +8,6 @@ class Task_ShowProgressBar(BasicTask):
         super().__init__(**kwargs)
         self.screen = None
 
-    def present_instructions(self, **kwargs) -> None:
+    def present_task(self, prompt, duration, **kwargs) -> None:
         self.screen = utils.load_slide(self.win, kwargs['slide_image'])
         self.show_text(screen=self.screen, msg="Task", audio=None, wait_time=kwargs['duration'], waitKeys=kwargs['wait_keys'])
