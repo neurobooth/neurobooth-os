51528a4ec7e361a924e5cbc1a527983f8dc1eb5c
Larry White <ljw1001@gmail.com>
2025-11-19 16:16:07 -0500

    Update task_video_break.py

--- neurobooth_os/tasks/task_video_break.py
+++ neurobooth_os/tasks/task_video_break.py
@@ -1,5 +1,7 @@
 from neurobooth_os.tasks.task_eyetracker import Eyelink_HostPC
 from neurobooth_os.tasks.utils import load_video
+import neurobooth_os.config as cfg
+from os import path as op
 
 
 class VideoBreak(Eyelink_HostPC):
@@ -14,5 +16,9 @@ class VideoBreak(Eyelink_HostPC):
         super().__init__(**kwargs)
 
     def present_stimulus(self, duration, **kwargs):
-        video = load_video(self.win, self.video_file)
+        path_video = op.join(
+            cfg.neurobooth_config.video_task_dir, self.video_file
+        )
+
+        video = load_video(self.win, path_video)
         self._show_video(video=video, msg="Task")
