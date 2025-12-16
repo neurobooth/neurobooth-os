ce3c5fcccf03b1b83f7afa5d17a51669792a8c94
Larry White <ljw1001@gmail.com>
2025-12-16 12:30:15 -0500

    Update stm_session.py

--- neurobooth_os/stm_session.py
+++ neurobooth_os/stm_session.py
@@ -43,7 +43,6 @@ class StmSession(BaseModel):
             self.session_name: str = kwargs["session_name"]
         self.collection_id = kwargs["collection_id"]
         self.selected_tasks = kwargs["selected_tasks"]
-        self.db_conn = kwargs["db_conn"]
         self.logger = kwargs["logger"]
         self.session_folder = self.create_session_folder(self.logger, self.session_name)
         self.system_resource_logger: SystemResourceLogger = self.create_sys_resource_logger()
