a60185763b9e56257dabc0501edfd634bbfa64dd
Larry White <ljw1001@gmail.com>
2026-01-13 17:26:47 -0500

    re-add changes lost in merge

--- neurobooth_os/server_acq.py
+++ neurobooth_os/server_acq.py
@@ -115,6 +115,9 @@ def run_acq(logger):
 
             elif "StartRecording" == current_msg_type:
                 msg_body: StartRecording = message.body
+                dev_id = msg_body.frame_preview_device_id
+                if dev_id is not None:
+                    camera_frame_preview(dev_id, device_manager, logger)
 
                 task = msg_body.task_id
                 fname = msg_body.fname
