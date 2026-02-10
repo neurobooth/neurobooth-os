ad0c4381ee321d7401e7b022da1576e2cfab1d50
Larry White <ljw1001@gmail.com>
2026-02-10 16:36:13 -0500

    clean up batch files

--- neurobooth_os/server_stm.bat
+++ neurobooth_os/server_stm.bat
@@ -1,2 +1,2 @@
-call  %NB_CONDA_INSTALL%\Scripts\activate.bat %NB_CONDA_ENV%
-start /W ipython --pdb %NB_INSTALL%\neurobooth_os\server_stm.py
+call %NB_CONDA_INSTALL%\Scripts\activate.bat %NB_CONDA_ENV%
+start python %NB_INSTALL%\neurobooth_os\server_stm.py
