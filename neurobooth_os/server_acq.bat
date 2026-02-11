83abb8d7c0bd511f3bd4a3e919826cc22ab77b70
Larry White <ljw1001@gmail.com>
2026-02-11 16:48:40 -0500

    remove -pdb flag

--- neurobooth_os/server_acq.bat
+++ neurobooth_os/server_acq.bat
@@ -1,2 +1,2 @@
 call %NB_CONDA_INSTALL%\Scripts\activate.bat %NB_CONDA_ENV%
-start /W ipython --pdb %NB_INSTALL%\neurobooth_os\server_acq.py
+start /W ipython %NB_INSTALL%\neurobooth_os\server_acq.py
